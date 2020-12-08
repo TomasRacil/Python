ecdf7698f256e97fe4a918ceef799352896c7845
TomasRacil <tomas.racil@unob.cz>
2020-12-08 19:05:34 +0100

    AoC 2 updated

--- Zaklady/Scripty/Vyuka2019KB/AdventOfCode/Racil/2/AoC2.py
+++ Zaklady/Scripty/Vyuka2019KB/AdventOfCode/Racil/2/AoC2.py
@@ -1,17 +1,28 @@
 import re
 
-hesla=[re.split(r'\W+',radek.strip()) for radek in open("hesla.txt", "r")]
+print(len([heslo for heslo in [re.split(r'\W+',radek.strip()) for radek in open("hesla.txt", "r")] 
+	if int(heslo[0])<=heslo[3].count(heslo[2])<=int(heslo[1])]))
+
+"""
+hesla = [re.split(r'\W+',radek.strip()) for radek in open("hesla.txt", "r")]
 
 pocet = 0
 for heslo in hesla:
-	if heslo[3].count(heslo[2])<=int(heslo[1]) and heslo[3].count(heslo[2])>=int(heslo[0]): pocet+=1
+	if int(heslo[0])<=heslo[3].count(heslo[2])<=int(heslo[1]): pocet+=1
 
 print(pocet)
+"""
+
+print(len([heslo for heslo in [re.split(r'\W+',radek.strip()) for radek in open("hesla.txt", "r")] 
+	if (heslo[3][int(heslo[0])-1]==heslo[2]) != (heslo[3][int(heslo[1])-1]==heslo[2])]))
+
+"""
+hesla = [re.split(r'\W+',radek.strip()) for radek in open("hesla.txt", "r")]
 
 pocet = 0
 
 for heslo in hesla:
-	print(heslo)
 	if (heslo[3][int(heslo[0])-1]==heslo[2]) != (heslo[3][int(heslo[1])-1]==heslo[2]): pocet+=1
 
 print(pocet)
+"""
