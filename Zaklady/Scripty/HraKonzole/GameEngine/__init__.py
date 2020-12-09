077c814486ac403f78892cc94ecb8eb4d4bd08c7
jindji987 <jiri.jindra989@seznam.cz>
2020-12-09 23:48:41 +0100

    navrat reakceNaNepritele

--- Zaklady/Scripty/HraKonzole/GameEngine/__init__.py
+++ Zaklady/Scripty/HraKonzole/GameEngine/__init__.py
@@ -4,3 +4,6 @@ from .kostka import hodKostkou
 from .ageExploit import *
 from .setup import *
 from .smer import zmenaSmeru
+from .reakceNaNepritele import *
+from .hrdinaUtok import *
+from .nepritelUtok import *
