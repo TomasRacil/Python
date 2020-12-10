55fb8e2c1ef96a6665ac6adc622aa99cb559631d
TomasRacil <tomas.racil@unob.cz>
2020-12-10 21:19:00 +0100

    AoC 5,6 commented

--- Zaklady/Scripty/Vyuka2019KB/AdventOfCode/Racil/5/AoC5.py
+++ Zaklady/Scripty/Vyuka2019KB/AdventOfCode/Racil/5/AoC5.py
@@ -1,9 +1,15 @@
-
+#vytváříme set letenek letenky musí být jedinečné proto je set ideální
 letenky={
-int("".join(["0" if char=="F" else "1" for char in letenka.strip()[:-3]]),2)*8
-+int("".join(["0" if char=="L" else "1" for char in letenka.strip()[-3:]]),2) 
-for letenka in open("letenky.txt", "r")}
+int("".join(["0" if char=="F" else "1" for char in letenka.strip()[:-3]]),2) #procházíme každý znak a vytváříme list nul a jedniček (F=0,B=1), následně tento list spojíme v řetězec a tento řetězec pomocí castingu převedeme na int int(str,2)  dva označuje že řetězec obsahuje číslo v binární formě
+*8 #vynásobíme získané binární číslo osmi
++int("".join(["0" if char=="L" else "1" for char in letenka.strip()[-3:]]),2) #podobné jako u prvního akorát L=0 R=1
+for letenka in open("letenky.txt", "r")} #procházíme soubor řádek po řádku
+#v setu máme pak uložené všechny letenky s jejich čísly převedenými z do desítkové soustavy
 
+#maximální hodnota v setu max()
 print(max(letenky))
 
+#vytvoříme set pomocí castingu a generátoru s rozsahem od nejnižší po nejvyšší letenku
+#následně uděláme rozdíle mezi tímto setem a letnkami tak se nám vrátí jediný prvek
+#abychom získali hodnotu kterou tento set obsahuje změníme ho na iterátor iter() a zavolám první element pomocí next()
 print(next(iter((set(range(min(letenky),max(letenky)))-letenky))))
--- Zaklady/Scripty/Vyuka2019KB/AdventOfCode/Racil/6/AoC6.py
+++ Zaklady/Scripty/Vyuka2019KB/AdventOfCode/Racil/6/AoC6.py
@@ -1,14 +1,20 @@
+
+#vytváříme list skupin, kde každá skupina obsahuje set jednotlivých osob; split pomocí dvojitých newline skupiny a jedné newline osoby 
 customs=[ [set(osoba) for osoba in skupina.split("\n")] for skupina in open("customs.txt", "r").read().split("\n\n")]
 
-vsechny=0
-stejne=0
+#
+vsechny, stejne=0,0
+#procházíme skupin po skupině
 for skupina in customs:
+	#nad každou skupinou provedeme operaci intersection která vrací set prvků společných všem setům; hvězdička slouží k odkázání na celou množinu
 	stejne+=len(set.intersection(*skupina))
+	#nad každou skupinou provedeme operaci union která vrací set všech jedinečných prvků těmto setům
 	vsechny+=len(set.union(*skupina))
+#vytiskneme součet všech délek setů 
 print(vsechny, stejne)
 
 
-
+#stejné jako předchozí varianta ale převedeno do list comprehension a funkce suma pro součet všech prvků v seznamu
 vsechny=sum([len(set.union(*[set(osoba) for osoba in skupina.split("\n")])) 
 	for skupina in open("customs.txt", "r").read().split("\n\n")])
 stejne=sum([len(set.intersection(*[set(osoba) for osoba in skupina.split("\n")])) 
