11f097adc74ab49cc7d771ab8be260d034a97567
TomasRacil <tomas.racil@unob.cz>
2020-12-10 20:26:16 +0100

    AoC 1,2 commented

--- Zaklady/Scripty/Vyuka2019KB/AdventOfCode/Racil/1/AoC1.py
+++ Zaklady/Scripty/Vyuka2019KB/AdventOfCode/Racil/1/AoC1.py
@@ -1,14 +1,23 @@
 #slovnik={ 2020-int(line.strip()):int(line.strip()) for line in open("2000.txt", "r")}
 
+#čteme soubor řádek po řádku a u každého řádku použijeme metodu strip, která odstraní všechny bílé znaky ze začátku a konce řetězce 
+#také ho převedeme na int pomocí castingu následně je ukládáme do seznamu
 cisla=[int(line.strip()) for line in open("2000.txt", "r")]
+#u každého prvku seznamu spočítáme rozdíl mezi 2020 a prvkem a výsledek uložíme do seznamu
 zbytky=[2020-cislo for cislo in cisla]
 
+#procházíme seznam a ověřujeme jestli jsou jednotlivé prvky v seznamu zbytky pokud ano vytiskneme čísla a jejich násobek
 for cislo in cisla:
 	if cislo in zbytky: 
 		print(f"prvni: {cislo}; druhe: {2020-cislo}; součin {cislo*(2020-cislo)}")
 		break
 
+#cyklus, který funguje podobně jako předchozí, ale přistupuje k souboru v rámci cyklu
+#prvně procházíme a najdeme zbytek po odečtení 2020 a čísla v souboru následně hledáme shodné číslo v souboru
+#pokud najdeme vynásobíme
 for cislo in open("2000.txt", "r"):
 	zbytek=2020-int(cislo.strip())
 	for cislo1 in open("2000.txt", "r"):
 		if zbytek==int(cislo1.strip()):	print(zbytek*int(cislo.strip()))
+
+#TODO použít indexování
--- Zaklady/Scripty/Vyuka2019KB/AdventOfCode/Racil/2/AoC2.py
+++ Zaklady/Scripty/Vyuka2019KB/AdventOfCode/Racil/2/AoC2.py
@@ -1,8 +1,11 @@
 import re
 
-print(len([heslo for heslo in [re.split(r'\W+',radek.strip()) for radek in open("hesla.txt", "r")] 
-	if int(heslo[0])<=heslo[3].count(heslo[2])<=int(heslo[1])]))
+print(len(														#tiskneme a zjišťujeme délku řetězce
+	[heslo for heslo in [re.split(r'\W+',radek.strip()) 		#upravujme jednotlivé řádky, rozdělujeme na jednotlivé skupiny znaků pomocí regulárního výrazu a vkládáme je do seznamu
+	for radek in open("hesla.txt", "r")] 						#procházíme testový soubor vracíme řádky pro další zpracování
+	if int(heslo[0])<=heslo[3].count(heslo[2])<=int(heslo[1])]))#podmínka která nechá v seznamu jen platná hesla, heslo[0] a heslo[1] reprezentuje počet písmen povolených v písmenu pomocí metody count pak počítáme počet konkrétních znaků (heslo[2]) v hesle (heslo[3])
 
+#verze vykonávajíc to samé ale pomocí cyklů místo list comprehension
 """
 hesla = [re.split(r'\W+',radek.strip()) for radek in open("hesla.txt", "r")]
 
@@ -13,9 +16,12 @@ for heslo in hesla:
 print(pocet)
 """
 
-print(len([heslo for heslo in [re.split(r'\W+',radek.strip()) for radek in open("hesla.txt", "r")] 
-	if (heslo[3][int(heslo[0])-1]==heslo[2]) != (heslo[3][int(heslo[1])-1]==heslo[2])]))
+print(len(
+	[heslo for heslo in [re.split(r'\W+',radek.strip()) 								#stejné jako v předchozím
+	for radek in open("hesla.txt", "r")] 												#stejné jako v předchozím
+	if (heslo[3][int(heslo[0])-1]==heslo[2]) != (heslo[3][int(heslo[1])-1]==heslo[2])]))#kontrolujeme jestli znak heslo[3] na indexu uvedeném v heslo[0] a heslo [1] odpovídá znaku heslo[2] nnásledně využíváme nerovnosti pro ověření že je výraz platný jen tehdy je-li pouze jedna odpověď platná
 
+#verze vykonávajíc to samé ale pomocí cyklů místo list comprehension
 """
 hesla = [re.split(r'\W+',radek.strip()) for radek in open("hesla.txt", "r")]
 
