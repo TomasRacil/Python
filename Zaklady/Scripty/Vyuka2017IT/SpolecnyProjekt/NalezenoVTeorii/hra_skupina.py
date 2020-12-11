8e71bc0c1a07ba076831960bcee8b65bab825efd
TomasRacil <tomas.racil@unob.cz>
2020-12-11 17:21:27 +0100

    Indentation changed to be coherent

--- Zaklady/Scripty/Vyuka2017IT/SpolecnyProjekt/NalezenoVTeorii/hra_skupina.py
+++ Zaklady/Scripty/Vyuka2017IT/SpolecnyProjekt/NalezenoVTeorii/hra_skupina.py
@@ -7,28 +7,28 @@ for otazka in otazky:
 	urovenSpokojenosti=0
 	print(otazka[0])
 	print(moznosti[0])
-    odpoved=input()
-    if odpoved=="D":
-    	print("Ti-ti-ticho!")
-    	urovenSpokojenosti=urovenSpokojenosti+1
-    else: print("Kecy,kecy,kecy!!!!!")
-    	urovenSpokojenosti=urovenSpokojenosti-1	
-    print(otazka[1])
-    print(moznosti[1])
-    odpoved=input()
-    if odpoved=="C":
-    	print("Ti-ti-ticho^2!")
-    	urovenSpokojenosti=urovenSpokojenosti+1
-    else: print("To je hnus!!!!!")		
-        urovenSpokojenosti=urovenSpokojenosti-1
-    print(otazka[2])
-    print(moznosti[2])
-    odpoved=input()  
-    if odpoved=="D":
-    	print("Ti-ti-ticho^3!")
-    	urovenSpokojenosti=urovenSpokojenosti+1
-    else: print("Kecy,kecy,kecy!!!!!")	  
-    	urovenSpokojenosti=urovenSpokojenosti-1
-    if urovenSpokojenosti>=2:
-    	print("Major je s vasi odpovedi spokojeny")
-    else: print("Majorovi se z vas chce zvracet")	
+	odpoved=input()
+	if odpoved=="D":
+		print("Ti-ti-ticho!")
+		urovenSpokojenosti=urovenSpokojenosti+1
+	else: print("Kecy,kecy,kecy!!!!!")
+		urovenSpokojenosti=urovenSpokojenosti-1	
+	print(otazka[1])
+	print(moznosti[1])
+	odpoved=input()
+	if odpoved=="C":
+		print("Ti-ti-ticho^2!")
+		urovenSpokojenosti=urovenSpokojenosti+1
+	else: print("To je hnus!!!!!")		
+		urovenSpokojenosti=urovenSpokojenosti-1
+	print(otazka[2])
+	print(moznosti[2])
+	odpoved=input()  
+	if odpoved=="D":
+		print("Ti-ti-ticho^3!")
+		urovenSpokojenosti=urovenSpokojenosti+1
+	else: print("Kecy,kecy,kecy!!!!!")	  
+		urovenSpokojenosti=urovenSpokojenosti-1
+	if urovenSpokojenosti>=2:
+		print("Major je s vasi odpovedi spokojeny")
+	else: print("Majorovi se z vas chce zvracet")	
