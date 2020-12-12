07cf0131c500349d0e964e6725a07e3b5938d013
Uživatelské jméno <z.vylezich.ml@seznam.cz>
2020-12-12 13:37:46 +0100

    AoC opr

--- Zaklady/Scripty/Vyuka2017IT/AdventOfCode/vylezich/2/ukol2.py
+++ Zaklady/Scripty/Vyuka2017IT/AdventOfCode/vylezich/2/ukol2.py
@@ -1,16 +1,9 @@
-import time
 import re
 soubor = open("Hesla.txt", "r").readlines()
-starttime = time.time()
 pocetplatnychhesel=0
 for radek in soubor:
-   print(re.split('[- :]', radek.strip()))
    parts=re.split('[- :]', radek.strip())
    if(int(parts[0]) <= parts[4].count(parts[2]) & parts[4].count(parts[2]) <= int(parts[1])):
       pocetplatnychhesel=pocetplatnychhesel+1
       print(radek)
-
-   ##cislo = Petr[2].count(Petr[1])
-   ##print(cislo)
 print(f"pocet platnych cisel :{pocetplatnychhesel}")
-print(f"{time.time()-starttime}\n")
