f054dff4998b3f78049053faabdd0c07496bef02
StastnyMi <mercenry451@seznam.cz>
2020-12-15 18:44:15 +0100

    ukol

--- Zaklady/Scripty/Vyuka2017IT/Ukoly/ukol_narozeniny.py
+++ Zaklady/Scripty/Vyuka2017IT/Ukoly/ukol_narozeniny.py
@@ -1,18 +1,39 @@
 """1. Program do kterého je možné uložit osobu a datum narození a v případě že má dnes někdo narozeniny tak na to při spuštění upozorní."""
-soubor1=open("datumy.txt","r").readlines()
-jmeno=input("Zadejte svoje jmeno a prijmeni:")
-datum_Narozeni=input("Zadejte datum narozeni:")
-for radek in soubor1:
-	"""print(radek)"""
-	radek.strip()
-	if radek==datum_Narozeni:
-		print("Gratuluji, mate dnes narozeniny!")
-	else:
-		with open('datumy.txt', mode='w', encoding='utf-8') as soubor1:	
-			print("\n")
-			print(datum_Narozeni,file=soubor1)	
-		print("Dnes nemate narozeniny")	
-with open('osoby.txt', mode='w', encoding='utf-8') as soubor2:	
-	print("\n")
-	print(jmeno,file=soubor2)	
+import datetime
 
+def vrat_Dnesni_Datum():
+	date=datetime.datetime.now()
+	den=format(date.day)
+	mesic=format(date.month)
+	rok=format(date.year)
+	dnesni_Datum=den+"."+mesic+"."+rok
+	dnesni_Datum=str(dnesni_Datum)
+	return dnesni_Datum
+
+soubor1=open("datumy.txt","r").readlines()
+soubor2=open("osoby.txt","r").readlines()
+jmeno=input("Zadejte svoje jmeno a prijmeni:")
+datum_Narozeni=input("Zadejte svoje datum narozeni:")
+dnes=vrat_Dnesni_Datum()
+if datum_Narozeni==dnes:
+		print("Gratuluji, mate dnes narozeniny!")
+else:
+	for radek in soubor1:
+		if radek!=datum_Narozeni:
+			print("Dnes nemate narozeniny!")
+			soubor1=open("datumy.txt","a")	
+			soubor1.write(datum_Narozeni+"\n")
+	
+for radek in soubor2:
+	if radek==jmeno:
+		print("Vase jmeno uz je v souboru")
+	elif radek!=jmeno:
+		print("Vase jmeno neni v souboru")
+		soubor2=open("osoby.txt","a")
+		soubor2.write(jmeno+"\n")
+	"""try:
+					radek==jmeno
+					print("Vase jmeno uz je v souboru")
+				except Exception as e:
+					print("Nastala {e}, vase jmeno neni v souboru")	
+					soubor2.write("\n"+jmeno)"""
