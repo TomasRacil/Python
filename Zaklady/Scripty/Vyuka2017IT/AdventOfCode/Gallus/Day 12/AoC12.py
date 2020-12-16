5ff45a8d618f37bb6a7a71578a09aca847840e8a
TomasRacil <tomas.racil@unob.cz>
2020-12-17 00:05:10 +0100

    Error solving

--- Zaklady/Scripty/Vyuka2017IT/AdventOfCode/Gallus/Day 12/AoC12.py
+++ Zaklady/Scripty/Vyuka2017IT/AdventOfCode/Gallus/Day 12/AoC12.py
@@ -8,25 +8,32 @@ E = orientace
 w = waypoint()
 
 class lod:
-    def rotace(course,degree)
+    def rotace(course,degree):
+        pass
 
-    def pohyb(command,value)
+    def pohyb(command,value):
+        pass
 
-    def poloha()
+    def poloha():
+        pass
 
-    def navigace(list)
+    def navigace(list):
+        pass
 
 
 
 
 
 #SECOND STAR
-    def rotace2(course,degree)
+    def rotace2(course,degree):
+        pass
 
-    def pohyb2(strana,value)
+    def pohyb2(strana,value):
+        pass
 
     def pohyb2(self,value):
         self.N += self.w.N*value
         self.E += self.w.E*value
 
     def navigace2(self,list):
+        pass
