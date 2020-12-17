8c02239172cd6cb53be9171e443ac9edd2f48a9e
TomasRacil <tomas.racil@unob.cz>
2020-12-17 10:54:52 +0100

    +data
    AoC12 lesson

---
+++ Zaklady/Scripty/Vyuka2017IT/PrikladyZHodin/AoC12.py
@@ -0,0 +1,35 @@
+class Ship:
+	rotace=90
+	poziceN=0
+	poziceE=0
+
+	def pohyb(self,smer,krok):
+		if smer=="N":self.poziceN+=krok
+		elif smer=="E":self.poziceE+=krok
+		elif smer=="S":self.poziceN-=krok
+		elif smer=="W":self.poziceE-=krok
+		elif smer=="F":
+			soucasnaRotace=self.rotace
+			if soucasnaRotace==0:self.poziceN+=krok
+			elif soucasnaRotace==90:self.poziceE+=krok
+			elif soucasnaRotace==180:self.poziceN-=krok
+			elif soucasnaRotace==270:self.poziceE-=krok
+
+	def orientace(self,smer,krok):
+		self.rotace+= krok if smer=="P" else -krok
+		if self.rotace<0: self.rotace=360+self.rotace
+		if self.rotace>=360: self.rotace=self.rotace-360
+
+	def navigace(self, navigacniList):
+		for krok in navigacniList:
+			if krok[0]=="L" or krok[0]=="P":
+				self.orientace(krok[0],krok[1])
+			else: self.pohyb(krok[0],krok[1])
+
+navigacniList=[[line.strip()[0],int(line.strip()[1:])] for line in open("navigacniList.txt","r")]
+for line in navigacniList:
+	print(line)
+lod=Ship()
+print(lod.poziceN,lod.poziceE,lod.rotace)
+lod.navigace(navigacniList)
+print(lod.poziceN,lod.poziceE,lod.rotace)
