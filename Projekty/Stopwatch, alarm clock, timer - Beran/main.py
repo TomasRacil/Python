17cbca6afea992763a0df9495f7932fb6bb95ec0
JanBeran8 <jan.beran@unob.cz>
2020-12-30 19:53:30 +0100

    final version of the project

--- Projekty/Stopwatch, alarm clock, timer - Beran/main.py
+++ Projekty/Stopwatch, alarm clock, timer - Beran/main.py
@@ -6,7 +6,8 @@ import winsound
 import time
 import datetime
 import math
-  
+from tkinter import messagebox
+
 """Clock - vypise aktualni cas, AM/PM, den,mesic a rok"""
 def time(): 
     string = strftime('%H:%M:%S %p \n %d.%m.%Y')
@@ -46,20 +47,11 @@ def countdown(count):
         root.after(1000, countdown,count-1)
 
     else:
-    	#Alert okno o vyprseni casu
-        alert = tkinter.Tk()
-        alert.title('Timer alert')
-        alert.geometry("240x160")
-        alert.resizable(False,False)
-        alert.configure(bg="silver")
-        labeltimer = Label(alert, text = "Time is up!",font=('arial',15,'bold'),background='silver',foreground='green')
-        labeltimer.place(x=65,y=60) 	
-
         #Zvukovy signal	
         for x in range(3):
-         winsound.Beep(500,1000)
-
-        alert.mainloop()
+            winsound.Beep(500,1000)
+            #Alert okno o vyprseni casu
+        messagebox.showinfo("Timer alert", "Time´s up!")
 
 """Start timer"""
 #isdigit - metoda vraci true, pokud jsou všechny znaky číslice
@@ -70,24 +62,59 @@ def updateButton():
            stp_time=int(hr)*3600+int(mn)*60+int(sec)
            countdown(stp_time)
 
+"""Stopwatch labels and buttons"""
 def stopwatchWidgets():
     stopwatch = Label(root, text="Stopwatch",font=('arial',30,'bold'),background="dimgray",foreground='black').pack(anchor = 'center')
     stopwatchLabel = Label(root, text='0:0:0',font=('arial',30,'bold'),foreground='green',background="black",width=6).place(x=110,y=260)
     button1=Button(root,text="Start",command=start).place(x=100,y=230)
     button2=Button(root,text="Stop",command=stop).place(x=180,y=230)
 
-#Stopwatch - definování proměnných
+"""Alarm clock labels and button"""
+def alarm():
+    global e1,e2
+    alarm_clock=Label(root,text="Alarm clock",font=('arial',30,'bold'),background="dimgray",foreground='black').place(x=60,y=520)
+    hours = Label(root,text="Hours: ",font=('arial',10,'bold'),background="lightgreen")
+    hours.place(x=100,y=580)
+    e1=Entry(root,width=10)
+    e1.place(x=170,y=580)
+    minutes=Label(root,text="Minutes: ",font=('arial',10,'bold'),background="lightgreen")
+    minutes.place(x=100,y=600)
+    e2 = Entry(root,width=10)
+    e2.place(x=170,y=600)
+    begin=Button(root,text="Start alarm")
+    begin.place(x=120,y=630)
+    begin.bind("<Button-1>",alarm_begin)
+
+"""Alarm clock """
+def alarm_begin(event):
+    global e1,e2
+
+    h=e1.get()
+    m=e2.get()
+
+#Pokud se rovna cas, ktery zadame, se soucasnym casem zazni zvukovy signal a zobrazi se upozorneni
+    while(1):
+        if (int(h)==datetime.datetime.now().hour and int(m)==datetime.datetime.now().minute):
+            winsound.Beep(2000,1000)
+            winsound.Beep(400,1000)
+            winsound.Beep(600,800)
+            messagebox.showinfo("Alarm alert", "Time´s up!")
+            break
+
+#Definování proměnných
 second=0
 minute=0
 hour=0
 stp=0
 entry1=0
 entry2=0
+e1=0
+e2=0
 
 #Hlavni dialogove okno
 root = tkinter.Tk() 
 root.title('Clock,alarm,stopwatch,timer') 
-root.geometry("360x560")
+root.geometry("360x700")
 root.resizable(False,False)
 root.configure(bg="dimgray")
 
@@ -99,6 +126,7 @@ lbl.pack(anchor = 'n')
 #Zavolame funkci time a funkci s widgety pro stopky
 time() 
 stopwatchWidgets()
+alarm()
 
 #Timer labels
 timer = Label(root, text="Timer",font=('arial',30,'bold'),background='dimgray',foreground='black').place(x=110,y=310)
@@ -116,10 +144,10 @@ secondT=tkinter.Label(root, text="Seconds:",font=('arial',10,'bold'),background=
 secondE=tkinter.Entry(root,width=10)
 secondE.place(x=170,y=450)
 
-button=tkinter.Button(root,text="Start Timer",command=updateButton).place(x=20,y=490)
+button=tkinter.Button(root,text="Start Timer",command=updateButton).place(x=120,y=480)
 
-#label pro otestování zda zbytek funguje
+#label pro otestování zda timer funguje
 label = tkinter.Label(root,background="dimgray")
-label.place(x=100,y=490)
+label.place(x=100,y=510)
 
 root.mainloop()
