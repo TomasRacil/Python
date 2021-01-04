c68bd11d50a76c999fabf6ba123c4ea8cc2750b0
StastnyMi <mercenry451@seznam.cz>
2021-01-04 22:13:44 +0100

    Web layout

---
+++ Projekty/Web_Layout/js/datum.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+var datum=new Date(); 
+var den=datum.getDate();
+var mesic=datum.getMonth()+1;
+var rok=datum.getFullYear();
+
+
+document.write(den+"."+mesic+"."+rok);
+
+
+
