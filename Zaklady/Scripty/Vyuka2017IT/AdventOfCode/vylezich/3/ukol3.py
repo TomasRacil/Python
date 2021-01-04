f83185d4ceb2ff1908c68565d2d2522e0c36921a
Uživatelské jméno <z.vylezich.ml@seznam.cz>
2021-01-04 16:20:33 +0100

    Sudoku

--- Zaklady/Scripty/Vyuka2017IT/AdventOfCode/vylezich/3/ukol3.py
+++ Zaklady/Scripty/Vyuka2017IT/AdventOfCode/vylezich/3/ukol3.py
@@ -1,23 +1,19 @@
 soubor = open("3.txt", "r").readlines()
 pocetStromu = 0
-index=0
-start = 3
-stav=0
-
+start = 0
+"""Prochazeni celého souboru a hledání, #"""
 for radek in soubor:
-    print(index)
-    if(index%11!= 0):
-        print(f"start: {start}")
-        if(radek[start]=='#'):
-            print(radek[start])
-            pocetStromu = pocetStromu+1
-            print(f"Pocet stromu je: {pocetStromu}")
-        if(start==28):
-            start = -3
-        if(start==29):
-            start = -2
-        if(start==30):
-            start = -1
-        start = start + 3
-    index = index + 1
+    if(radek[start]=='#'):
+        pocetStromu = pocetStromu+1
+        """Podmínky pro průchod námi chtěné cesty a znovunastaevení"""
+    if(start==28):
+        start = -3
+    if(start==29):
+        start = -2
+    if(start==30):
+        start = -1
+    start = start + 3
+    """Výsledný počet stromů"""
+
+    """Autorské řešení:"""
 print(f"Pocet stromu po tomhle cyklu je: {pocetStromu}")
