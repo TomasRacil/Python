b6c13444772646779f51332bfbae9f220f776a47
Uživatelské jméno <z.vylezich.ml@seznam.cz>
2021-01-06 19:26:43 +0100

    sudoku

--- Projekty/Sudoku/SudokuGenerator.py
+++ Projekty/Sudoku/SudokuGenerator.py
@@ -8,13 +8,14 @@ from time import sleep
 bod = []
 for radek in range(0,9):
     bod.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 myPen = turtle.Turtle()
 myPen._tracer(0) #rovnou zobrazít celé sudoku
 myPen.speed(0) #0 zajistí maximální rychlost vypisování
 myPen.color("black")
 myPen.hideturtle()
-topLeft_x = -150#Jenom pomocna, pro zobrazení na monitoru
+topLeft_x = -150  # Jenom pomocna, pro zobrazení na monitoru
 topLeft_y = 150
 
 
@@ -49,3 +50,34 @@ def drawbod(bod):
         for slo in range(0, 9):
             if bod[rad][slo] != 0:
                 text(bod[rad][slo], topLeft_x + slo * intDim + 9, topLeft_y - rad * intDim - intDim + 8, 20)
+
+def naplnBod(bod):
+    global counter
+    for i in range(0, 81):
+        rad = i // 9
+        slo = i % 9
+        if bod[rad][slo] == 0:
+            shuffle(numberList)
+            for value in numberList:
+                if not (value in bod[rad]):
+
+
+def resBod(bod):
+    global counter
+
+def vymaz(bod, obtiznost):
+
+
+def main():
+    obtiznost = 10*int(input("Zadejte obtiznost (1-10): \n"))
+    drawbod(bod)
+    counter = 1
+    sleep(5)
+    print("Sudoku bod Ready")
+    for slopec in range(0,9):
+            print(f"{bod[slopec][0]} {bod[slopec][1]} {bod[slopec][2]} | {bod[slopec][3]} {bod[slopec][4]} {bod[slopec][5]} | {bod[slopec][6]} {bod[slopec][7]} {bod[slopec][8]}")
+            if (slopec % 3 == 2):
+                print("_____________________")
+
+if __name__ == '__main__':
+    main()
