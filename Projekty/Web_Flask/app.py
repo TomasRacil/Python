d595f086e2f3ace58c8714014eda33143f5cdb00
StastnyMi <mercenry451@seznam.cz>
2021-01-12 19:54:48 +0100

    flask

---
+++ Projekty/Web_Flask/app.py
@@ -0,0 +1,36 @@
+# import the Flask class from the flask module
+from flask import Flask, render_template, redirect, url_for, request
+import datetime,html
+
+# create the application object
+app = Flask(__name__)
+
+# use decorators to link the function to a url
+@app.route('/')
+def vrat_Dnesni_Datum():
+	date=datetime.datetime.now()
+	den=format(date.day)
+	mesic=format(date.month)
+	rok=format(date.year)
+	dnesni_Datum=den+"."+mesic+"."+rok
+	dnesni_Datum=str(dnesni_Datum)
+	return dnesni_Datum  # return a string
+
+@app.route('/home')
+def welcome():
+    return render_template('index.html') # render a template    
+# Route for handling the login page logic
+
+@app.route('/login.html', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Vaše údaje nesouhlasí. Zkuste to prosím znovu.'
+        else:
+            return render_template('dlsc09.html')
+    return render_template('login.html', error=error)
+
+# start the server with the 'run()' method
+if __name__ == '__main__':
+    app.run(debug=True)
