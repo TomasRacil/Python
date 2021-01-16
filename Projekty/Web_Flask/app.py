fd0b51ae0046279596a716c2578a0ca974396627
StastnyMi <mercenry451@seznam.cz>
2021-01-16 21:43:49 +0100

    flask bootstrap sqlalchemy

--- Projekty/Web_Flask/app.py
+++ Projekty/Web_Flask/app.py
@@ -1,36 +1,91 @@
-# import the Flask class from the flask module
-from flask import Flask, render_template, redirect, url_for, request
-import datetime,html
 
-# create the application object
+from flask import Flask, render_template, redirect, url_for
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm 
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
+from flask_sqlalchemy  import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+import datetime
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Thisissupposedtobesecret!'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/42060/Desktop/UNOB/4. ročník/UdIS/Flask/database.db'
+bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
-# use decorators to link the function to a url
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(15), unique=True)
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(80))
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+    remember = BooleanField('remember me')
+
+class RegisterForm(FlaskForm):
+    email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+
+
 @app.route('/')
-def vrat_Dnesni_Datum():
-	date=datetime.datetime.now()
-	den=format(date.day)
-	mesic=format(date.month)
-	rok=format(date.year)
-	dnesni_Datum=den+"."+mesic+"."+rok
-	dnesni_Datum=str(dnesni_Datum)
-	return dnesni_Datum  # return a string
+def index():
+    date=datetime.datetime.now()
+    den=format(date.day)
+    mesic=format(date.month)
+    rok=format(date.year)
+    dnesni_Datum=den+"."+mesic+"."+rok
+    dnesni_Datum=str(dnesni_Datum)
+    return render_template('index.html',datum=dnesni_Datum)
 
-@app.route('/home')
-def welcome():
-    return render_template('index.html') # render a template    
-# Route for handling the login page logic
-
-@app.route('/login.html', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Vaše údaje nesouhlasí. Zkuste to prosím znovu.'
-        else:
-            return render_template('dlsc09.html')
-    return render_template('login.html', error=error)
+    form = LoginForm()
 
-# start the server with the 'run()' method
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if check_password_hash(user.password, form.password.data):
+                login_user(user, remember=form.remember.data)
+                return redirect(url_for('index'))
+
+        return '<h1>Neplatné uživatelské jméno nebo heslo</h1>'
+        #return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
+
+    return render_template('login.html', form=form)
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        hashed_password = generate_password_hash(form.password.data, method='sha256')
+        new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return '<h1>Nový uživatel  vytvořen!</h1>'
+        #return '<h1>' + form.username.data + ' ' + form.email.data + ' ' + form.password.data + '</h1>'
+
+    return render_template('reg.html', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
 if __name__ == '__main__':
     app.run(debug=True)
