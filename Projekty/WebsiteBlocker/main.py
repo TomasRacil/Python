0f3b9ddfa0927e13c26c4dd8222b6dede7f145f3
BalgaMatej <matej.balga00@gmail.com>
2021-01-26 11:24:46 +0100

    script

---
+++ Projekty/WebsiteBlocker/main.py
@@ -0,0 +1,45 @@
+# Run this script as root 
+  
+import time 
+from datetime import datetime as dt 
+  
+# change hosts path according to your OS 
+hosts_path = "C:\Windows\System32\drivers\etc"
+# localhost's IP 
+redirect = "127.0.0.1"
+  
+# websites That we want to block 
+website_list =["www.facebook.com","facebook.com","www.gmail.com","gmail.com"] 
+  
+while True: 
+  
+    # time of your work 
+    if dt(dt.now().year, dt.now().month, dt.now().day,8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day,18): 
+        print("Working hours...") 
+        with open(hosts_path, 'r+') as file: 
+            content = file.read() 
+            for website in website_list: 
+                if website in content: 
+                    pass
+                else: 
+                    # mapping hostnames to your localhost IP address 
+                    file.write(redirect + " " + website + "\n") 
+    else: 
+        with open(hosts_path, 'r+') as file: 
+            content=file.readlines() 
+            file.seek(0) 
+            for line in content: 
+                if not any(website in line for website in website_list): 
+                    file.write(line) 
+  
+            # removing hostnmes from host file 
+            file.truncate() 
+  
+        print("Fun hours...") 
+    time.sleep(5) 
+
+
+#127.0.0.1 www.instagram.com
+#127.0.0.1 instagram.com
+#127.0.0.1 www.facebook.com
+#127.0.0.1 facebook.com
