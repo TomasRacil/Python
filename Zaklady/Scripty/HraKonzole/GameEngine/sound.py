7819e079f6b888fcba936bdbd469be80f5734493
Hrab-Jos <75065432+Hrab-Jos@users.noreply.github.com>
2021-02-01 11:41:52 +0100

    Update sound.py

--- Zaklady/Scripty/HraKonzole/GameEngine/sound.py
+++ Zaklady/Scripty/HraKonzole/GameEngine/sound.py
@@ -1,1 +1,25 @@
 from playsound import playsound
+from GameEngine import *
+
+def ZvukKostka():
+	playsound('GameEngine/soundlib/kostka.wav')
+
+
+def ZvukBandita():
+	playsound('GameEngine/GameEngine/soundlib/bandita.wav')
+
+
+def ZvukDrak():
+	playsound('GameEngine/soundlib/drak.wav')
+
+
+def ZvukRychlost():
+	playsound('GameEngine/soundlib/rychlost.wav')
+
+
+def ZvukVoda():
+	playsound('GameEngine/soundlib/voda.wav')
+
+
+def ZvukSmrt():
+	playsound('GameEngine/soundlib/smrt.wav')
