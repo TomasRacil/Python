476a3d9e3414e0bf14197cf53fad77a31514ccca
TomasRacil <tomas.racil@unob.cz>
2021-02-17 10:29:18 +0100

    Modified YT Downloader

--- Projekty/YouTube - downloader/Source/downloadyt.py
+++ Projekty/YouTube - downloader/Source/downloadyt.py
@@ -1,4 +1,6 @@
 from pytube import YouTube
+import ffmpeg
+import os
 
 def downloader(odkaz, rozliseni, titulky):
 
@@ -19,22 +21,41 @@ def downloader(odkaz, rozliseni, titulky):
 		print("Žádné titulky  k dispozici")
 
 	if rozliseni == 'nezadano':													#pokud uživatel nezadá rozlišení, stáhne se video v nejlepší kvalitě
-		video_file = yt.streams.order_by('resolution').desc().first()		
+		video_file = yt.streams.filter(mime_type="video/webm").order_by('resolution').desc().first()		
 	else:
-		video_file = yt.streams.order_by('resolution').desc().filter(res=rozliseni)
+		video_file = yt.streams.filter(mime_type="video/webm").filter(res=rozliseni).order_by('resolution').desc().first()
 
 	audio_file = yt.streams.filter(only_audio=True).order_by('abr').desc().first()		
 
+
+	if not os.path.exists(os.getcwd()+"/tmp"):
+			os.makedirs(os.getcwd()+"/tmp")
+
+	if not os.path.exists(os.getcwd()+"/downloaded_videos"):
+			os.makedirs(os.getcwd()+"/downloaded_videos")
+
 	try: 
 		print("Stahování")
-		video_file.download()    						 	#zahájí stahování videa a zvukobé stopy
-		audio_file.download(filename_prefix="audio_") 		
+		video_file.download(output_path=os.getcwd()+"/tmp",filename="video")    						 	#zahájí stahování videa a zvukobé stopy
+		audio_file.download(output_path=os.getcwd()+"/tmp",filename="audio") 		
 
+		print(os.getcwd()+"/tmp/audio.webm")
+		source_audio = ffmpeg.input(os.getcwd()+"\\tmp\\audio.webm")			#zpracování videa a stopy
+		source_video = ffmpeg.input(os.getcwd()+"\\tmp\\video.webm")
 
-		source_audio = ffmpeg.input(audio_file)			#zpracování videa a stopy
-		source_video = ffmpeg.input(video_file)
+		ffmpeg.concat(source_video, source_audio, v=1, a=1).output("downloaded_videos/"+yt.title+".mp4").run(cmd=os.getcwd()+"\\Source\\binaries\\ffmpeg")
 
-		ffmpeg.concat(source_video, source_audio, v=1, a=1).output("Stažené_video.mp4").run()
 		print("Staženo")
+
 	except:
 		print("Špatně zadaná data")
+
+
+	if os.path.exists(os.getcwd()+"/tmp/video.webm"):
+			os.remove(os.getcwd()+"/tmp/video.webm")
+
+	if os.path.exists(os.getcwd()+"/tmp/audio.webm"):
+		os.remove(os.getcwd()+"/tmp/audio.webm")
+
+	if os.path.exists(os.getcwd()+"/tmp"):
+		os.rmdir(os.getcwd()+"/tmp")
