b5db5baf9a56c4c9a63d64d5d940562a614447ff
jindji987 <jiri.jindra989@seznam.cz>
2021-02-21 20:31:17 +0100

    projekt update

--- Projekty/Notification/main.py
+++ Projekty/Notification/main.py
@@ -2,12 +2,12 @@ from moduls.notifie import *
 from moduls.serverRequest import *
 import time
 
-data = serverRequest()
+data = serverRequest() #fukce nacte a vrati data ze serveru 
 if data != None:
-	lastDeath = data['deceased']
-	notifie("Dosavadní počet úmrtí na Covid-19",data['deceased'])
+	lastDeath = data['deceased'] #vytahneme z dat pocet smrti
+	notifie("Dosavadní počet úmrtí na Covid-19",data['deceased']) #vytvori se notifikaci s poctem smrti
 	while True:
-		time.sleep(600)
-		data = serverRequest()
-		if lastDeath < data['deceased']:
+		time.sleep(600) #pocka se 10 minut
+		data = serverRequest() #znova se nactou data ze serveru
+		if lastDeath < data['deceased']: #pokovad se pocet mrtvich zvysil vytvori se notifikace s narustem
 			notifie("Počet mrtvích na Covid-19 se zvýšil o",data['deceased']-lastDeath)
--- Projekty/Notification/moduls/serverRequest.py
+++ Projekty/Notification/moduls/serverRequest.py
@@ -2,9 +2,11 @@ import requests
 import json
 
 def serverRequest():
-	try:
+	try: 
+		#funkce se pokusi ziskat data ze serveru
 		r = requests.get('https://api.apify.com/v2/key-value-stores/K373S4uCFR9W1K8ei/records/LATEST?disableRedirect=true')
 	except:
+		#pokovad se to nepovede vypise zpravu
 		print('Nelze se připojit k serveru')
 		r = None
 	return json.loads(r.text)
