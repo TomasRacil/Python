22e8f1cc23c5240b1fe3dc519371b851cb815246
jindji987 <jiri.jindra989@seznam.cz>
2021-02-23 11:10:48 +0100

    projekt komentare

--- Projekty/Notification/moduls/notifie.py
+++ Projekty/Notification/moduls/notifie.py
@@ -1,7 +1,10 @@
 from win10toast import ToastNotifier
 
-#funkce vytvari windows 10 notifikaci s nadpisem a zpravou kterou dostala
-#jako ikonka notifikace se pouzije obrazek ulozeny s programem
+"""
+vstupy jsou dva stringy
+funkce vytvari windows 10 notifikaci s nadpisem a zpravou kterou dostala
+jako ikonka notifikace se pouzije obrazek ulozeny s programem
+"""
 def notifie(title, message):
 	toaster = ToastNotifier()
 	toaster.show_toast(title, str(message), icon_path="./skull.ico", duration=10)
--- Projekty/Notification/moduls/serverRequest.py
+++ Projekty/Notification/moduls/serverRequest.py
@@ -1,12 +1,16 @@
 import requests
 import json
 
+"""
+funkce se pokusi ziskat data z api serveru
+data ziska ve forme javascript objektu
+a pak je prevede na python list
+"""
 def serverRequest():
-	try: 
-		#funkce se pokusi ziskat data ze serveru
+	try:
 		r = requests.get('https://api.apify.com/v2/key-value-stores/K373S4uCFR9W1K8ei/records/LATEST?disableRedirect=true')
 	except:
-		#pokovad se to nepovede vypise zpravu
+		#pokovad se request nepovede vypise zpravu
 		print('Nelze se připojit k serveru')
 		r = None
 	return json.loads(r.text)
