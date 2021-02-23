0b06d3f6af5e82c495f516741156089e6201163c
Hrab-Jos <75065432+Hrab-Jos@users.noreply.github.com>
2021-02-23 12:15:09 +0100

    Update downloadyt.py

--- Projekty/YouTube - downloader/Source/downloadyt.py
+++ Projekty/YouTube - downloader/Source/downloadyt.py
@@ -3,48 +3,61 @@ import ffmpeg
 import os
 
 def downloader(odkaz, rozliseni, titulky):
+	"""
+	Funkce která najde pomocí odkazu youtube video a následně dle dalších poždavků stáhne a upraví:
+	
+	1) Najde video
+	2) Stáhne titulky
+	3) Zvlášť se stáhne video a zvuková stopa
+	4) Pokud nejsou, vytvoří se potřebné složky pro manipulaci a uložení
+	5) Zahájí se stahování videa a stopy
+	6) Uloží se ve formátu "webm"
+	7) Zpracuje video a zvuk do formátu "mp4"
+	8) Odstraní složky pro manipulaci
+	
+	Následně bude ve složce "downloaded_videos" stažené video v požadované kvalitě	
+	"""
 
-	yt = YouTube(odkaz) 	 #najde video
+	yt = YouTube(odkaz) 	 		# 1)
 
-	print("Název: ",yt.title)			#vypíše paramtery videa
+	print("Název: ",yt.title)			
 	print("Délka videa: ",yt.length)
 	print("Počet shlédnutí: ",yt.views)
 
 
 	try:
-		if titulky == 'ano': #stahování tutulků
-			caption = yt.captions.get_by_language_code('en')  #zvolení jazyka, zatím nevolitelné => vždy angličtina
+		if titulky == 'ano': 					# 2) 
+			caption = yt.captions.get_by_language_code('en') 
 			caption.xml_captions
 			tit = caption.generate_srt_captions()
 			tit.download()
 	except:
 		print("Žádné titulky  k dispozici")
 
-	if rozliseni == 'nezadano':	#pokud uživatel nezadá rozlišení, stáhne se první nabízené video
+	if rozliseni == 'nezadano':  						# 3) 
 		video_file = yt.streams.filter(mime_type="video/webm").order_by('resolution').desc().first()		
-	else: 				#zvlášť se seáhne video a zvuková stopa
+	else: 		
 		video_file = yt.streams.filter(mime_type="video/webm").filter(res=rozliseni).order_by('resolution').desc().first()    
-		#zvuková stopa je jen jedna pro všechan videa stejná
 	audio_file = yt.streams.filter(only_audio=True).order_by('abr').desc().first()		
 
 
-	if not os.path.exists(os.getcwd()+"/tmp"):  #pokud nejsou, vytvoří potřebné složky
+	if not os.path.exists(os.getcwd()+"/tmp"):                  # 4)
 			os.makedirs(os.getcwd()+"/tmp")
 
 	if not os.path.exists(os.getcwd()+"/downloaded_videos"):
 			os.makedirs(os.getcwd()+"/downloaded_videos")
 
 	try: 
-		print("Stahování")  #zahájí stahování videa a zvukové stopy
+		print("Stahování")  						# 5)
 		video_file.download(output_path=os.getcwd()+"/tmp",filename="video")  
 		audio_file.download(output_path=os.getcwd()+"/tmp",filename="audio") 		
 
-		print(os.getcwd()+"/tmp/audio.webm")  #uloží ve formátu webm
+		print(os.getcwd()+"/tmp/audio.webm")  				#6)
 		source_audio = ffmpeg.input(os.getcwd()+"\\tmp\\audio.webm")			
 		source_video = ffmpeg.input(os.getcwd()+"\\tmp\\video.webm")
 
-		#zpracování videa a stopy do mp4
-
+		
+							# 7)
 		ffmpeg.concat(source_video, source_audio, v=1, a=1).output("downloaded_videos/"+yt.title+".mp4").run(cmd=os.getcwd()+"\\Source\\binaries\\ffmpeg")
 
 		print("Staženo")
@@ -52,7 +65,7 @@ def downloader(odkaz, rozliseni, titulky):
 	except:
 		print("Špatně zadaná data")
 
-		#postupně odstraní nepotřebné složky
+		#8) postupně odstraní nepotřebné složky
 	if os.path.exists(os.getcwd()+"/tmp/video.webm")		
 			os.remove(os.getcwd()+"/tmp/video.webm")
 
