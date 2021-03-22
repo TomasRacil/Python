ec2ffb1f616c3c9d8345e45189a8c4841b744982
Andabates <75065517+Andabates@users.noreply.github.com>
2021-03-23 00:25:19 +0100

    Update wiki.py

--- Projekty/RandomWiki/wiki.py
+++ Projekty/RandomWiki/wiki.py
@@ -4,9 +4,9 @@ import webbrowser
 
 
 while True:
-    address = requests.get("https://en.wikipedia.org/wiki/Special:Random")#získá html adresu random článku
-    html = BeautifulSoup(address.content, 'html.parser')#html kód
-    title = html.find(id = "firstHeading").text#získá titulek
+    address = requests.get("https://en.wikipedia.org/wiki/Special:Random")  #získá html adresu náhodného článku pomocí odkazu
+    html = BeautifulSoup(address.content, 'html.parser')                    #získá html kód článku
+    title = html.find(id = "firstHeading").text                             #získá titulek článku
     print(title + "\nChcete tento článek? (Y/N)")
     ans = str(input(""))
     if (ans.lower() == "y"):
@@ -14,7 +14,6 @@ while True:
         webbrowser.open(url)
         break
     elif (ans.lower() == "n"):
-        #print("\nHledám jiný")
         continue
     else:
         print("Špatná odpověď!")
