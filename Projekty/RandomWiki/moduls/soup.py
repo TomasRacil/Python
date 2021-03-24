e89d896684f7acc751cb74f6b83b2fb910e684d4
andabates <andabates@seznam.cz>
2021-03-24 09:55:30 +0100

    Projekt - random wiki - Schejbal

--- Projekty/RandomWiki/moduls/soup.py
+++ Projekty/RandomWiki/moduls/soup.py
@@ -3,13 +3,17 @@ from bs4 import BeautifulSoup
 import webbrowser
 
 def soup():
+    """Funkce pomocí odkazu https://en.wikipedia.org/wiki/Special:Random
+    načte náhodný článek v html kódu, vypíše titulek a první odstavec
+    a nabídne jeho zobrazení ve webovém prohlížeči
+    """
     while True:
         address = requests.get("https://en.wikipedia.org/wiki/Special:Random")      #získá html adresu náhodného článku pomocí odkazu
         html = BeautifulSoup(address.content, 'html.parser')                        #získá html kód článku
         title = html.find(id = "firstHeading").text                                 #získá titulek článku
-        odstavec = html.find_all('p')                                               #získá první odstavec v html
-        print(title + "\n\n" + odstavec[0].text + "\nChcete tento článek? (Y/N)")   #získá první odstavec textu
-        ans = str(input(""))
+        odstavec = html.find_all('p')                                               #získá odstavece v html
+        print(title + "\n\n" + odstavec[0].text + "\nChcete tento článek? (Y/N)")   #získá první odstavec textu 
+        ans = str(input(""))                                                        # a společně s titulkem je vypíše
         if (ans.lower() == "y"):
             url = 'https://en.wikipedia.org/wiki/%s' %title
             webbrowser.open(url)
