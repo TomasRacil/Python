31be2c78c5a57bc5905e81a99a9f5dd9b1ce9e3f
TomasRacil <tomas.racil@unob.cz>
2021-11-24 19:48:57 +0100

    Drobné úpravy algoritmů

--- Zaklady/Scripty/Vyuka2018IT/PrvniHodina/1.py
+++ Zaklady/Scripty/Vyuka2018IT/PrvniHodina/1.py
@@ -2,11 +2,12 @@
 jmeno=input("Zadej křestní jméno: ")
 prijmeni=input("Zadej prijmeni: ")
 
-for number in range(1,101):
+for number in range(0,101):
     if(number%len(jmeno+prijmeni)==0):
         if (number%len(jmeno)==0):
-            print("X")
-        print(number*2)
+            print(number) if number==0 else print("X")
+        else:
+            print(number*2)
     elif (number%len(jmeno)==0):
             pass
     else:
