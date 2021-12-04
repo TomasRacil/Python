a1ddf85f0d0f1d3d9010f9baf90f8c5a14829041
TomasRacil <tomas.racil@unob.cz>
2021-12-04 12:47:56 +0100

    sesta hodina API post fixed

--- Zaklady/Scripty/Vyuka2018IT/SestaHodina/CvicnaApi/main.py
+++ Zaklady/Scripty/Vyuka2018IT/SestaHodina/CvicnaApi/main.py
@@ -1,10 +1,16 @@
-from flask import Flask
-from flask_restx import Api, Resource
+from re import DEBUG
+from flask import Flask, request
+from flask_restx import Api, Resource, reqparse
+import logging
+
+logging.basicConfig(level=DEBUG)
 
 app = Flask(__name__)
 api = Api(app)
 
-
+# parser = reqparse.RequestParser()
+# parser.add_argument('rate', type=str, help='World to change')
+# args = parser.parse_args()
 
 @api.route('/hello')
 class HelloWorld(Resource):
@@ -13,9 +19,10 @@ class HelloWorld(Resource):
 
     def get(self):
         return {'hello': self.test}
-    def put(self, data):
-        self.test=data
-        return {'hello': data}
+    def post(self):
+        self.test=request.form['data']
+        logging.debug(request.form['data'])
+        return {'hello': self.test}
 
 @api.route('/test')
 class Test(Resource):
