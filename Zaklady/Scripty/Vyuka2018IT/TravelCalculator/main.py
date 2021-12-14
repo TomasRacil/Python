52b2c77cb816c40ab581640dac1b578d70583373
TomasRacil <tomas.racil@unob.cz>
2021-12-14 10:24:10 +0100

    API hodina

--- Zaklady/Scripty/Vyuka2018IT/TravelCalculator/main.py
+++ Zaklady/Scripty/Vyuka2018IT/TravelCalculator/main.py
@@ -17,7 +17,7 @@ class Vehicle:
 class Route:
     def __init__(self,vehicles, startLocation, endLocation,startTime, endTime=None):
         self.vehicles=vehicles
-        self.startLocation=startLocation
+        self.startLocation = startLocation
         self.endLocation = endLocation
         self.startTime = startTime
         self.distance = endLocation-startLocation
@@ -36,44 +36,36 @@ class Route:
     def cenaVsech(self,cenaNafty, cenaBenzinu):
         return self.spotrebaVsech()*cenaNafty
 
-prvniAuto=Vehicle(90, 0, 8, 'diesel')
-druheAuto=Vehicle(120, 240, 10, 'benzin')
+# prvniAuto=Vehicle(90, 0, 8, 'diesel')
+# druheAuto=Vehicle(120, 240, 10, 'benzin')
 
-prvniCesta = Route([prvniAuto,druheAuto],0,640, "10:00")
-prvniCesta.info()
-print(prvniCesta.spotrebaVsech())
-print(prvniCesta.cenaVsech(35,35))
+# prvniCesta = Route([prvniAuto,druheAuto],0,640, "10:00")
+# prvniCesta.info()
+# print(prvniCesta.spotrebaVsech())
+# print(prvniCesta.cenaVsech(35,35))
 
 
 api_key ='AlphaDMAVhNcGaWYPjbHanGWO0MJyTQqV0HnPVeb'
   
-# Take source as input
-source = 'Praha'
+source = input("Zadej výchozí bod")
   
-# Take destination as input
-dest = 'Brno'
+dest = input('Zadej cílový bod')
 
 departure = 'now'
-#"https://maps.distancematrixapi.com/maps/api/distancematrix/json?origins=Boston%2CMA%7CCharlestown%2CMA&destinations=Lexington%2CMA%7CConcord%2CMA&departure_time=now&key=AlphaDMAVhNcGaWYPjbHanGWO0MJyTQqV0HnPVeb"
-# url variable store url 
 url ='https://maps.distancematrixapi.com/maps/api/distancematrix/json?'
-  
-# Get method of requests module
-# return response object
-#r=requests.get(url)
+
 r = requests.get(url + 'origins=' + source +
                    '&destinations=' + dest +
                    '&departure_time=' + departure +
                    '&key=' + api_key)
                      
-# json method of response object
-# return json format result
 x = r.json()
+print(x['rows'][0]['elements'][0]['distance']['value'])
   
 # by default driving mode considered
   
 # print the value of x
-print(x)
+#print("vyd8lenost v metrech: ",x['rows'][0]['elements'][0]['distance']['value'])
 # dobaCesty=(prvniAuto.x+druheAuto.x)/(prvniAuto.avgSpeed+druheAuto.avgSpeed)
 # hodiny=int(dobaCesty)
 # minuty=int(60/((dobaCesty-hodiny)*100))
