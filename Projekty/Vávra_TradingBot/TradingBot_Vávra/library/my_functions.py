13886f8b69397d111a8ef086f6896bee7a117747
arktocz <86154324+arktocz@users.noreply.github.com>
2022-01-04 09:18:42 +0100

    Update my_functions.py

--- Projekty/Vávra_TradingBot/TradingBot_Vávra/library/my_functions.py
+++ Projekty/Vávra_TradingBot/TradingBot_Vávra/library/my_functions.py
@@ -30,6 +30,7 @@ file_handler.setLevel(logging.DEBUG)
 logger.addHandler(stream_handler)
 logger.addHandler(file_handler)
 
+#JUST OPTIONS TO IMMIDIATELY CREATE ORDER TO PROVE ITS WORKING
 #client2=BinanceFuturesClient(config.API_KEY, config.API_SECRET, True)
 #client2.place_order(TRADE_SYMBOL, "BUY", 0.01, "LIMIT", 20000, "GTC")
 
