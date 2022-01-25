628d29679d1dc4769547be5bde65907b3e620c45
TomasRacil <tomas.racil@unob.cz>
2022-01-25 17:19:22 +0100

    YoutubeDownloaderUpdated

--- Projekty/YouTube - downloader GUI/YT/main.py
+++ Projekty/YouTube - downloader GUI/YT/main.py
@@ -1,4 +1,5 @@
-import os.path
+from os.path import dirname, abspath, exists, join
+from os import makedirs, remove, rmdir, rename
 
 import ffmpeg
 import PySimpleGUI as sg
@@ -8,7 +9,8 @@ sg.theme('DarkAmber')
 
 layout = [
     [sg.Text("Zadejte YouTube odkaz"), sg.InputText(key='url')],
-    [sg.Radio("Video", "RADIO1", key="VID", default=True), sg.Radio("Zvuk", "RADIO1", key="AUD")],
+    [sg.Radio("Video", "RADIO1", key="VID", default=True),
+     sg.Radio("Zvuk", "RADIO1", key="AUD")],
     [sg.Text("Zadejte složku"), sg.InputText(key='path'), sg.FolderBrowse()],
     [sg.Button('Potvrdit vše')]
 ]
@@ -28,71 +30,53 @@ while True:
     elif values["VID"] and not win2_active:
 
         vid_url = values['url']
-        path = values['path']
+        target_path = values['path']
         try:
             yt = YouTube(vid_url)
+            resolutions = set(
+                map(lambda x: x.resolution,
+                    yt.streams.filter(mime_type="video/webm")
+                    )
+            )
+            captions = {x.name: x.code for x in yt.captions.all()}
+            captions_name = list(captions.keys())
 
-            captions_en = ''
-            captions_cs = ''
+            # captions_en = ''
+            # captions_cs = ''
 
-            try:
-                captions_en = yt.captions.get_by_language_code('en')
-            except:
-                pass
-            try:
-                captions_cs = yt.captions.get_by_language_code('cs')
-            except:
-                pass
+            # try:
+            #     captions_en = yt.captions.get_by_language_code('en')
+            # except Exception as e:
+            #     print(e)
+            # try:
+            #     captions_cs = yt.captions.get_by_language_code('cs')
+            # except Exception as e:
+            #     print(e)
 
             win2_active = True
             window.hide()
 
-        except:
+        except Exception as e:
+            print(e)
             values['url'] = ""
             sg.popup_error("Zadejte url!")
             continue
 
-        #GUI pro titulky ještě nemám vyřešené, ale vím jak na to, jde si jen o to najít čas to udělat
-        if not captions_en and not captions_cs:
-            layout2 = [
-                [sg.Text("Zadejte rozlišení videa (720p,480p,etc)"), sg.Combo(key='RES',
-                                                              values=(
-                                                                  '1080p', '720p', '480p', '360p', '240p', '144p'))],
-                [sg.Text("Titulky nejsou k dispozici")],
-                [sg.Button('Potvrdit vše', key='BTN')]
-            ]
-            win2 = sg.Window('Select res and sub', layout2)
+        # GUI pro titulky ještě nemám vyřešené, ale vím jak na to,
+        # jde si jen o to najít čas to udělat
 
-        elif not captions_cs:
-            layout2 = [
-                [sg.Text("Zadejte rozlišení videa"), sg.Combo(key='RES',
-                                                              values=(
-                                                                  '1080p', '720p', '480p', '360p', '240p', '144p'))],
-                [sg.Text("Titulky: ")],
-                [sg.Listbox(values=('Ne', captions_en), size=(30, 6), key='List')],
-                [sg.Button('Potvrdit vše', key='BTN')]
-            ]
-            win2 = sg.Window('Druhe okno', layout2)
-        elif not captions_en:
-            layout2 = [
-                [sg.Text("Zadejte rozlišení videa (720p,480p,etc)"), sg.Combo(key='RES',
-                                                              values=(
-                                                                  '1080p', '720p', '480p', '360p', '240p', '144p'))],
-                [sg.Text("Titulky: ")],
-                [sg.Listbox(values=('Ne', captions_cs), size=(30, 6), key='List')],
-                [sg.Button('Potvrdit vše', key='BTN')]
-            ]
-            win2 = sg.Window('Druhe okno', layout2)
-        else:
-            layout2 = [
-                [sg.Text("Zadejte rozlišení videa (720p,480p,etc)"), sg.Combo(key='RES',
-                                                              values=(
-                                                                  '1080p', '720p', '480p', '360p', '240p', '144p'))],
-                [sg.Text("Titulky: ")],
-                [sg.Listbox(values=('Ne', captions_en, captions_cs), size=(30, 6), key='List')],
-                [sg.Button('Potvrdit vše', key='BTN')]
-            ]
-            win2 = sg.Window('Druhe okno', layout2)
+        layout2 = [
+            [sg.Text("Zadejte rozlišení videa: "),
+                sg.Combo(key='RES',
+                         values=list(resolutions)
+                         )
+             ],
+            [sg.Text("Titulky: ")],
+            [sg.Listbox(values=['ne']+captions_name,
+                        size=(30, 6), key='List')],
+            [sg.Button('Potvrdit vše', key='BTN')]
+        ]
+        win2 = sg.Window('Druhe okno', layout2)
 
         while True:
             ev2, vals2 = win2.read()
@@ -104,81 +88,68 @@ while True:
             if ev2 == 'BTN':
 
                 res = vals2['RES']
-                video = yt.streams.filter(res=res).first()
+                sub = vals2['List'][0]
+                video = yt.streams.filter(res=res).filter(
+                    mime_type="video/webm").first()
+                audio = yt.streams.filter(
+                    only_audio=True).order_by('abr').desc().first()
 
-                #video = yt.streams.filter(mime_type="video/webm").filter(res=res).order_by(
-                #   'resolution').desc().first()
-
-                if video is None:
-                    sg.popup_error('Zadaná kvalita není k dispozici')
-                elif res == '':
+                if res == '':
                     sg.popup_error('Zadejte kvalitu!')
+                elif sub == '':
+                    sg.popup_error('Zadejte titulky')
                 else:
-                    sub = ''
-
-                    try:
-                        sub = vals2['List']
-                    except:
-                        pass
-
-                    try:
-                        if "en" in str(sub[0]):
-                            code = 'en'
-                        elif "cs" in str(sub[0]):
-                            code = 'cs'
-                        else:
-                            code = 'ne'
-                    except:
-                        code = 'ne'
-
-                    #Nefungující titulky
+                    # Nefungující titulky
+                    # Koukněte se na tenhle odkaz:
+                    # https://stackoverflow.com/questions/68780808/xml-to-srt-conversion-not-working-after-installing-pytube
                     ###########################################################
-                    if 'ne' not in code:
-                        caption = yt.captions.get_by_language_code(code)
-                        caption.xml_captions
-                        tit = caption.generate_srt_captions()
-                        tit.download()
+                    if sub != 'ne':
+                        caption = yt.captions.get_by_language_code(
+                            captions[sub])
+                        print(caption.xml_captions)
+                        # tit = caption.generate_srt_captions()
+                        # caption.xml_captions.download()
                     ###########################################################
+                    script_path = dirname(abspath(__file__))
+                    if not exists(join(script_path, 'tmp')):
+                        makedirs(join(script_path, 'tmp'))
+                    video.download(output_path=join(script_path, 'tmp'),
+                                   filename="video.webm")
+                    audio.download(output_path=join(script_path, 'tmp'),
+                                   filename="audio.webm")
+                    source_audio = ffmpeg.input(
+                        join(script_path, 'tmp', 'audio.webm'))
+                    source_video = ffmpeg.input(
+                        join(script_path, 'tmp', 'video.webm'))
+                    ffmpeg.concat(source_video, source_audio, v=1, a=1).output(
+                        join(target_path, yt.title + '.mp4')).run(
+                        cmd=join(script_path, 'tools', 'ffmpeg'),
+                        overwrite_output=True)
+                    try:
+                        remove(join(script_path, 'tmp', 'audio.webm'))
+                        remove(join(script_path, 'tmp', 'video.webm'))
+                        rmdir(join(script_path, 'tmp'))
+                    except Exception as e:
+                        print(e)
+                    ###########################################################
+                    # audio = yt.streams.filter(only_audio=True).order_by(
+                    # 'abr').desc().first()
 
-                    video.download(output_path=path, filename=yt.title + ".mp4")
-
-
-                    ###############################################################
-                    # audio = yt.streams.filter(only_audio=True).order_by('abr').desc().first()
-                    #
-                    # if not os.path.exists(os.getcwd() + "/tmp"):
-                    #      os.makedirs(os.getcwd() + "/tmp")
-
-                    # try:
-                    #     print('Stahovani')
-                    #     video.download(filename='video.webm')
-                    #     audio.download(filename='audio.webm')
-                    #
-                    #     source_audio = ffmpeg.input("audio.webm")
-                    #     source_video = ffmpeg.input("video.webm")
-                    #
-                    #     ffmpeg.concat(source_video, source_audio, v=1, a=1).output(yt.title + ".mp4").run(
-                    #         cmd=os.getcwd() + "\\Source\\binaries\\ffmpeg")
-                    #
-                    # except:
-                    #     print("Bad data")
-
-
-                    ##################################################################
+                    ###########################################################
 
     elif values['AUD']:
 
         vid_url = values['url']
-        path = values['path']
+        target_path = values['path']
 
         yt = YouTube(vid_url)
 
         ys = yt.streams.get_audio_only()
-        soubor = ys.download(path)
+        soubor = ys.download(target_path)
 
-        base, ext = os.path.splitext(soubor)
+        base, ext = target_path.splitext(soubor)
         new_soubor = base + '.mp3'
-        os.rename(soubor, new_soubor)
+        rename(soubor, new_soubor)
 
         print("Hotovo")
 
