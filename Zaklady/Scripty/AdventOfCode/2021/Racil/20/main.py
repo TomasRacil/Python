e65005f6ef34f1cfd0d957ed3810275d9c9f986a
TomasRacil <tomas.racil@unob.cz>
2022-01-27 16:32:53 +0100

    AoC20

--- Zaklady/Scripty/AdventOfCode/2021/Racil/20/main.py
+++ Zaklady/Scripty/AdventOfCode/2021/Racil/20/main.py
@@ -3,40 +3,43 @@ from os import path
 
 def printList(list):
     for line in list:
-        
-        print(''.join(line).replace('0','.').replace('1','#'))
+
+        print(''.join(line).replace('0', '.').replace('1', '#'))
 
 
-def findNeighbors(list, x, y):
+def findNeighbors(list, x, y, char):
     directions = [(-1, -1), (0, -1), (1, -1),
                   (-1, 0), (0, 0), (1, 0),
                   (-1, 1), (0, 1), (1, 1)]
     binary = ''
     for direction in directions:
-        if 0 <= direction[1]+y and direction[1]+y < len(list) and 0 <= direction[0]+x and direction[0]+x < len(list[0]):
+        if 0 <= direction[1]+y and direction[1]+y < len(list)\
+                and 0 <= direction[0]+x and direction[0]+x < len(list[0]):
             binary += list[direction[1]+y][direction[0]+x]
         else:
-            binary += '0'
+            binary += char
     return int(binary, 2)
 
 
-def expandField(list):
-    radek = ['0' for item in range(len(list[0]))]
-    return [['0']+row+['0']for row in [radek]+list+[radek]]
+def expandField(list, char):
+    radek = [char for item in range(len(list[0]))]
+    return [[char]+row+[char]for row in [radek]+list+[radek]]
+
 
 def countOnes(image):
-    val=0
+    val = 0
     for row in image:
-        val+=row.count('1')
+        val += row.count('1')
     return val
 
 
-def enhanceImage(image,value_res):
-    image = expandField(image)
+def enhanceImage(image, value_res, char):
+    image = expandField(image, char)
+
     new_image = [[None for col in row] for row in image]
     for idy, row in enumerate(image):
         for idx, column in enumerate(row):
-            val = findNeighbors(image, idx, idy)
+            val = findNeighbors(image, idx, idy, char)
             symbol = value_res[val]
             new_image[idy][idx] = symbol
     return new_image
@@ -44,20 +47,17 @@ def enhanceImage(image,value_res):
 
 with open(path.join(
         path.dirname(path.realpath(__file__)),
-        "test.txt"), "r") as file:
+        "input.txt"), "r") as file:
     data = [item for item in file.read().split('\n\n')]
     value_res = ['1' if item ==
                  '#' else '0' for item in data[0] if item != "\n"]
     image = [['1' if symbol == '#' else '0' for symbol in line]
              for line in data[1].split("\n")]
 
-print(countOnes(image))
 
-image=enhanceImage(image, value_res)
-image=enhanceImage(image, value_res)
-# image=enhanceImage(image, value_res)
-print()
-print()
-
-#printList(image)
+i = 0
+while i < 50:
+    image = enhanceImage(
+        image, value_res, value_res[0] if i % 2 == 0 else value_res[255])
+    i += 1
 print(countOnes(image))
