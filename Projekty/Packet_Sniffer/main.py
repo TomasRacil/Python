e3ee4eed181a0f8b174a47d8b0a2b3b1c4b273a4
JakubP1 <jack.penkala99@gmail.com>
2022-02-07 09:11:21 +0100

    Update main.py

--- Projekty/Packet_Sniffer/main.py
+++ Projekty/Packet_Sniffer/main.py
@@ -9,13 +9,22 @@ from gui import GUI
 
 sniffer =None
 
-# Program entry point - it initializes and declares start, pause and closing threading functions 
+  
+    """ Programs entry point - it initializes and declares start, pause and closing threading functions 
+    """
 
 def initSniffer():
+    """ Function - Initializes sniffer object on script start and on every single sniffing session
+	  
+    """
     global sniffer
     sniffer = Sniffer(graphicalQueue, fileQueue,interface)
 
 def startSniffing(recorderInstance: Recorder, snifferInstance: Sniffer):
+    """ Function - Starts sniffer and recorder and initializes pauseFlags 
+	    Args:
+		    parameters: recorderInstance: Recorder, snifferInstance: Sniffer
+    """
     if recorder.is_alive():
         recorder.pauseFlag = False
         sniffer.pauseFlag = False
@@ -31,13 +40,19 @@ def startSniffing(recorderInstance: Recorder, snifferInstance: Sniffer):
         sniffer.start()
 
 def pauseSniffing(recorderInstance: Recorder, snifferInstance: Sniffer):
-
+    """ Function - Pause sniffer and recorder and by setting pauseFlags to False, sleep function is called so that packets remaining in buffer can be stored into csv file 
+	    Args:
+		    parameters: recorderInstance: Recorder, snifferInstance: Sniffer
+    """
     sniffer.pauseFlag = True
     sleep(0.2)
     recorder.pauseFlag = True
 
 def closeProgram(recorderInstance: Recorder, snifferInstance: Sniffer):
-    
+    """ Function - Called on program exit. It stops recorder and sniffer by setting flag to True and closes threads 
+	    Args:
+		    parameters: recorderInstance: Recorder, snifferInstance: Sniffer
+    """
     pauseSniffing(recorder, sniffer)
     sniffer.stopFlag = True
     recorder.stopFlag = True
