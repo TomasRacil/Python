5c7c6ec6a9ff9cfd6c665ff21d7ccd2bcfacc7df
Dosko987 <doskocilek@gmail.com>
2022-02-11 09:49:24 +0100

    Final

--- Projekty/YouTube - downloader GUI/YT/main.py
+++ Projekty/YouTube - downloader GUI/YT/main.py
@@ -1,9 +1,96 @@
 from os.path import dirname, abspath, exists, join
 from os import makedirs, remove, rmdir, rename
 
-import ffmpeg
+import os
 import PySimpleGUI as sg
 from pytube import YouTube
+import ffmpeg
+
+#ffmpeg-python
+#kompilovany ffmpeg tools
+#zmena knihovny titulku
+
+#################################################################################################
+
+def zmeneni_title():
+    title = yt.title.replace('/', '')
+    title = title.replace('\\', '')
+    title = title + '.mp4'
+    return title
+
+
+#################################################################################################
+
+def stazeni_titulku():
+    caption = yt.captions.get_by_language_code(
+        captions[sub])
+    print(caption.xml_captions)
+    tit = caption.generate_srt_captions()
+    with open(target_path + '/titulky.srt', 'w') as f:
+        f.writelines(tit)
+
+
+#################################################################################################
+
+def vytvoreni_tmp():
+    script_path = dirname(abspath(__file__))
+    if not exists(join(script_path, 'tmp')):
+        makedirs(join(script_path, 'tmp'))
+    return script_path
+
+
+#################################################################################################
+
+def videoaudio():
+    #Download
+    video.download(output_path=join(script_path, 'tmp'),
+                   filename="video.webm")
+    audio.download(output_path=join(script_path, 'tmp'),
+                   filename="audio.webm")
+
+#######################################################
+    #Spojeni
+    source_audio = ffmpeg.input(
+        join(script_path, 'tmp', 'audio.webm'))
+    source_video = ffmpeg.input(
+        join(script_path, 'tmp', 'video.webm'))
+
+    title = zmeneni_title()
+
+    ffmpeg.concat(source_video, source_audio, v=1, a=1).output(
+        join(target_path, title)).run(
+        cmd=join(script_path, 'tools', 'ffmpeg'),
+        overwrite_output=True)
+
+
+#################################################################################################
+
+def odstraneni_tmp():
+    try:
+        remove(join(script_path, 'tmp', 'audio.webm'))
+        remove(join(script_path, 'tmp', 'video.webm'))
+        rmdir(join(script_path, 'tmp'))
+    except Exception as e:
+        print(e)
+
+
+#################################################################################################
+
+def stazeni_audia():
+    vid_url = values['url']
+    target_path = values['path']
+
+    yt = YouTube(vid_url)
+
+    ys = yt.streams.get_audio_only()
+    soubor = ys.download(target_path)
+
+    base, ext = os.path.splitext(soubor)
+    new_soubor = base + '.mp3'
+    rename(soubor, new_soubor)
+
+
+#################################################################################################
 
 sg.theme('DarkAmber')
 
@@ -19,6 +106,8 @@ window = sg.Window("Hello Word", layout)
 
 win2_active = False
 
+#################################################################################################
+
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED:
@@ -31,6 +120,7 @@ while True:
 
         vid_url = values['url']
         target_path = values['path']
+
         try:
             yt = YouTube(vid_url)
             resolutions = set(
@@ -41,18 +131,6 @@ while True:
             captions = {x.name: x.code for x in yt.captions.all()}
             captions_name = list(captions.keys())
 
-            # captions_en = ''
-            # captions_cs = ''
-
-            # try:
-            #     captions_en = yt.captions.get_by_language_code('en')
-            # except Exception as e:
-            #     print(e)
-            # try:
-            #     captions_cs = yt.captions.get_by_language_code('cs')
-            # except Exception as e:
-            #     print(e)
-
             win2_active = True
             window.hide()
 
@@ -62,21 +140,22 @@ while True:
             sg.popup_error("Zadejte url!")
             continue
 
-        # GUI pro titulky ještě nemám vyřešené, ale vím jak na to,
-        # jde si jen o to najít čas to udělat
+        #################################################################################################
 
         layout2 = [
             [sg.Text("Zadejte rozlišení videa: "),
-                sg.Combo(key='RES',
-                         values=list(resolutions)
-                         )
+             sg.Combo(key='RES',
+                      values=list(resolutions)
+                      )
              ],
             [sg.Text("Titulky: ")],
-            [sg.Listbox(values=['ne']+captions_name,
+            [sg.Listbox(values=['ne'] + captions_name,
                         size=(30, 6), key='List')],
             [sg.Button('Potvrdit vše', key='BTN')]
         ]
         win2 = sg.Window('Druhe okno', layout2)
+
+        #################################################################################################
 
         while True:
             ev2, vals2 = win2.read()
@@ -88,7 +167,11 @@ while True:
             if ev2 == 'BTN':
 
                 res = vals2['RES']
-                sub = vals2['List'][0]
+                try:
+                    sub = vals2['List'][0]
+                except:
+                    sub = ''
+
                 video = yt.streams.filter(res=res).filter(
                     mime_type="video/webm").first()
                 audio = yt.streams.filter(
@@ -97,60 +180,20 @@ while True:
                 if res == '':
                     sg.popup_error('Zadejte kvalitu!')
                 elif sub == '':
-                    sg.popup_error('Zadejte titulky')
+                    sg.popup_error('Zadejte titulky!')
                 else:
-                    # Nefungující titulky
-                    # Koukněte se na tenhle odkaz:
-                    # https://stackoverflow.com/questions/68780808/xml-to-srt-conversion-not-working-after-installing-pytube
-                    ###########################################################
-                    if sub != 'ne':
-                        caption = yt.captions.get_by_language_code(
-                            captions[sub])
-                        print(caption.xml_captions)
-                        # tit = caption.generate_srt_captions()
-                        # caption.xml_captions.download()
-                    ###########################################################
-                    script_path = dirname(abspath(__file__))
-                    if not exists(join(script_path, 'tmp')):
-                        makedirs(join(script_path, 'tmp'))
-                    video.download(output_path=join(script_path, 'tmp'),
-                                   filename="video.webm")
-                    audio.download(output_path=join(script_path, 'tmp'),
-                                   filename="audio.webm")
-                    source_audio = ffmpeg.input(
-                        join(script_path, 'tmp', 'audio.webm'))
-                    source_video = ffmpeg.input(
-                        join(script_path, 'tmp', 'video.webm'))
-                    ffmpeg.concat(source_video, source_audio, v=1, a=1).output(
-                        join(target_path, yt.title + '.mp4')).run(
-                        cmd=join(script_path, 'tools', 'ffmpeg'),
-                        overwrite_output=True)
-                    try:
-                        remove(join(script_path, 'tmp', 'audio.webm'))
-                        remove(join(script_path, 'tmp', 'video.webm'))
-                        rmdir(join(script_path, 'tmp'))
-                    except Exception as e:
-                        print(e)
-                    ###########################################################
-                    # audio = yt.streams.filter(only_audio=True).order_by(
-                    # 'abr').desc().first()
 
-                    ###########################################################
+                    if sub != 'ne':
+                        stazeni_titulku()
+
+                    script_path = vytvoreni_tmp()
+
+                    videoaudio()
+
+                    odstraneni_tmp()
 
     elif values['AUD']:
 
-        vid_url = values['url']
-        target_path = values['path']
-
-        yt = YouTube(vid_url)
-
-        ys = yt.streams.get_audio_only()
-        soubor = ys.download(target_path)
-
-        base, ext = target_path.splitext(soubor)
-        new_soubor = base + '.mp3'
-        rename(soubor, new_soubor)
-
-        print("Hotovo")
+        stazeni_audia()
 
 window.close()
