18d859291313dcde44185411a0af43e7a1a6a25a
Megalekk <filip.zindler@seznam.cz>
2022-02-15 12:43:51 +0100

    praw

---
+++ Projekty/kindl zindler richter/bot.py
@@ -0,0 +1,11 @@
+import praw
+
+reddit = praw.Reddit(
+    client_id="my client id",
+    client_secret="my client secret",
+    user_agent="my user agent",
+    username="my username",
+    password="my password",
+)
+
+print(reddit.read_only)
