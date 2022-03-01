f10fffad4fc3e19cd4fc2058629165350db3ad01
Hribikri <95184853+Hribikri@users.noreply.github.com>
2022-03-01 09:46:08 +0100

    Update test.py

--- Projekty/hribikova-texteditor/test.py
+++ Projekty/hribikova-texteditor/test.py
@@ -13,11 +13,9 @@ class Editor(Window, Functions):
 
 
 if __name__ == "_main_":
-    main()
-
-root = Tk()
-root.geometry("1250x720")
-app = Editor(root)
-app.mainloop()
-# objekt Tk uložíme do root, nastavíme velikost okna, třídu Editor uložíme do proměnné app
-# pomocí mainloop řekneme app aby se spustila
+    root = Tk()
+    root.geometry("1250x720")
+    app = Editor(root)
+    app.mainloop()
+    # objekt Tk uložíme do root, nastavíme velikost okna, třídu Editor uložíme do proměnné app
+    # pomocí mainloop řekneme app aby se spustila
