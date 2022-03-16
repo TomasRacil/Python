59955ad8ba5daa4daabdb618d8a906df44bdc8a1
blazkoval <95191594+blazkoval@users.noreply.github.com>
2022-03-16 21:25:39 +0100

    operace s výroky kompletní

--- Projekty/VyrokoveTabulky/formule.py
+++ Projekty/VyrokoveTabulky/formule.py
@@ -5,28 +5,32 @@ def Neg(a):
     c = [0,0,0,0,0,0,0,0]
     x = 0
     while (x != len(c)):
-        
+        if (a[x]==0): c[x] = 1
+        else: c[x] = 0
         x+=1
     return c
 def Kon(a, b):
     c = [0,0,0,0,0,0,0,0]
     x = 0
     while (x != len(c)):
-        
+        if (a[x]==1 and b[x]==1): c[x] = 1
+        else: c[x] = 0       
         x+=1
     return c
 def Dis(a, b):
     c = [0,0,0,0,0,0,0,0]
     x = 0
     while (x != len(c)):
-        
+        if (a[x]==0 and b[x]==0): c[x] = 0
+        else: c[x] = 1        
         x+=1
     return c
 def Imp(a, b):
     c = [0,0,0,0,0,0,0,0]
     x = 0
     while (x != len(c)):
-        
+        if (a[x]==1 and b[x]==0): c[x] = 0
+        else: c[x] = 1        
         x+=1
     return c
 def Ekv(a, b):
@@ -42,5 +46,19 @@ p = [0,0,0,0,1,1,1,1]
 q = [0,0,1,1,0,0,1,1]
 r = [0,1,0,1,0,1,0,1]
 
+# - negace, + konjunkce, / disjunkce, > implikace, = ekvivalence
+#-p+q/((r>-q)=p)
+formule = Dis(Kon(Neg(p),q),(Ekv(Imp(r,Neg(q)),p)))
+print(formule)
+"""
+result = Neg(p)
+print(result)
+result = Kon(p,q)
+print(result)
+result = Dis(p,q)
+print(result)
+result = Imp(p,q)
+print(result)
 result = Ekv(p,q)
 print(result)
+"""
