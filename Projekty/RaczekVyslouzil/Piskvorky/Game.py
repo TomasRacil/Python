19815d03bd3a1cb66665b7cb2c9142ccfa330424
SadEuropean <95198116+SadEuropean@users.noreply.github.com>
2022-04-01 15:12:20 +0200

    Update Game.py

--- Projekty/RaczekVyslouzil/Piskvorky/Game.py
+++ Projekty/RaczekVyslouzil/Piskvorky/Game.py
@@ -2,11 +2,15 @@ from Functions import *
 from Results import *
 
 
-def play(screen, file, save):
+def play(screen, file):
     my_font = Font('Road_Rage.otf', 50)
     menu = Menus(screen)
     menu.menu(file)
-    domain = create_domain(screen, 3)
+    file.seek(0)
+    diff = int(file.read())
+    array_size = 3
+    win_condition = 3
+    domain = create_domain(screen, array_size)
     pygame.mouse.set_pos(screen.get_width() / 2, 0)
     while True:
         clock = pygame.time.Clock()
@@ -15,19 +19,17 @@ def play(screen, file, save):
         level_number = 1
         number_of_levels = 5
         while level:
-            turn = update_domain(screen, domain, my_font, turn, level_number, number_of_levels)
+            turn = update_domain(screen, domain, my_font, turn, level_number, number_of_levels, diff)
             catch = draw(domain)
-            catch = win(level_number, domain, 3, 3, catch)
+            catch = win(level_number, domain, array_size, win_condition, catch)
             if not catch == 0:
-                turn = ai_move(domain, turn, 3, 3)
+                turn = ai_move(domain, turn, array_size, win_condition, diff)
             else:
                 turn += 1
             if catch == 0:
                 level_number += 1
-            catch = win(level_number, domain, 3, 3, catch)
-
-            level_number = result(screen, domain, my_font, turn, catch, file, 3, level_number, number_of_levels)
+            catch = win(level_number, domain, array_size, win_condition, catch)
+            level_number, turn, diff = result(screen, domain, my_font, turn, catch, file, 3, level_number, number_of_levels, diff)
             game_input(file)
-            print(clock.get_fps())
             pygame.display.update()
             clock.tick(60)
