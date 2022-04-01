d5e57fe474a2f564c29dae07520933cba74133f5
SadEuropean <95198116+SadEuropean@users.noreply.github.com>
2022-04-01 15:16:38 +0200

    Update Functions.py

--- Projekty/RaczekVyslouzil/Piskvorky/Functions.py
+++ Projekty/RaczekVyslouzil/Piskvorky/Functions.py
@@ -18,7 +18,7 @@ def game_input(file):
                 sys.exit()
 
 
-def ai_move(game_array, turn, array_size, win_condition):
+def ai_move(game_array, turn, array_size, win_condition, diff):
     if turn % 2 == 0:
         x1 = -1
         y1 = -1
@@ -26,13 +26,19 @@ def ai_move(game_array, turn, array_size, win_condition):
         position = -1
         win_condition = win_condition - 1
         # bot play 1
-        if turn == 2:
+        if turn == 2 and int(diff) >= 2:
             if game_array[1][1].get_text() == 'X':
                 x1 = random.randint(0, 1) * 2
                 y1 = random.randint(0, 1) * 2
             else:
                 x1 = 1
                 y1 = 1
+        elif turn == 2:
+            while True:
+                x1 = random.randint(0, 2)
+                y1 = random.randint(0, 2)
+                if game_array[y1][x1].get_text() == '':
+                    break
         else:
             for y in range(array_size):
                 for x in range(array_size):
@@ -45,6 +51,7 @@ def ai_move(game_array, turn, array_size, win_condition):
                         y1 = y
                 num = 0
                 position = -1
+
             for x in range(array_size):
                 for y in range(array_size):
                     if game_array[y][x].get_text() == 'X':
@@ -56,6 +63,7 @@ def ai_move(game_array, turn, array_size, win_condition):
                         y1 = position
                 num = 0
                 position = -1
+
             for p in range(array_size):
                 if game_array[p][p].get_text() == 'X':
                     num += 1
@@ -66,16 +74,18 @@ def ai_move(game_array, turn, array_size, win_condition):
                     y1 = position
             num = 0
             position = -1
+
             for p in range(array_size):
-                if game_array[p][2 - p].get_text() == 'X':
+                if game_array[p][array_size - 1 - p].get_text() == 'X':
                     num += 1
-                if game_array[p][2 - p].get_text() == '':
+                if game_array[p][array_size - 1 - p].get_text() == '':
                     position = p
                 if num == win_condition and not position == -1:
-                    x1 = 2 - position
+                    x1 = array_size - 1 - position
                     y1 = position
             num = 0
             position = -1
+
             for y in range(array_size):
                 for x in range(array_size):
                     if game_array[y][x].get_text() == 'O':
@@ -87,6 +97,7 @@ def ai_move(game_array, turn, array_size, win_condition):
                         y1 = y
                 num = 0
                 position = -1
+
             for x in range(array_size):
                 for y in range(array_size):
                     if game_array[y][x].get_text() == 'O':
@@ -98,6 +109,7 @@ def ai_move(game_array, turn, array_size, win_condition):
                         y1 = position
                 num = 0
                 position = -1
+
             for p in range(array_size):
                 if game_array[p][p].get_text() == 'O':
                     num += 1
@@ -108,15 +120,19 @@ def ai_move(game_array, turn, array_size, win_condition):
                     y1 = position
             num = 0
             position = -1
+
             for p in range(array_size):
-                if game_array[p][2 - p].get_text() == 'O':
+                if game_array[p][array_size - 1 - p].get_text() == 'O':
                     num += 1
-                if game_array[p][2 - p].get_text() == '':
+                if game_array[p][array_size - 1 - p].get_text() == '':
                     position = p
                 if num == win_condition and not position == -1:
-                    x1 = 2 - position
+                    x1 = array_size - 1 - position
                     y1 = position
-            if x1 == -1 and y1 == -1 and draw(game_array) != 2:
+
+            if draw(game_array) == 2:
+                return 2
+            if x1 == -1 and y1 == -1:
                 while True:
                     x1 = random.randint(0, 2)
                     y1 = random.randint(0, 2)
@@ -134,15 +150,10 @@ def win(level_number, game_array, array_size, win_condition, catch):
 
     for i in range(array_size):
         for j in range(array_size):
-            if game_array[i][j].get_text() == '':
-                number_of_x = 0
-                number_of_o = 0
             if game_array[i][j].get_text() == 'O':
-                number_of_x = 0
                 number_of_o += 1
             if game_array[i][j].get_text() == 'X':
                 number_of_x += 1
-                number_of_o = 0
             if number_of_x == win_condition:
                 if level_number % 5 == 0:
                     return 1
@@ -150,21 +161,15 @@ def win(level_number, game_array, array_size, win_condition, catch):
                     return 0
             if number_of_o == win_condition:
                 return 3
-            if j == array_size - 1:
-                number_of_x = 0
-                number_of_o = 0
+        number_of_x = 0
+        number_of_o = 0
 
     for i in range(array_size):
         for j in range(array_size):
-            if game_array[j][i].get_text() == '':
-                number_of_x = 0
-                number_of_o = 0
             if game_array[j][i].get_text() == 'O':
-                number_of_x = 0
                 number_of_o += 1
             if game_array[j][i].get_text() == 'X':
                 number_of_x += 1
-                number_of_o = 0
             if number_of_x == win_condition:
                 if level_number % 5 == 0:
                     return 1
@@ -172,20 +177,14 @@ def win(level_number, game_array, array_size, win_condition, catch):
                     return 0
             if number_of_o == win_condition:
                 return 3
-            if j == array_size - 1:
-                number_of_x = 0
-                number_of_o = 0
+        number_of_x = 0
+        number_of_o = 0
 
     for i in range(array_size):
-        if game_array[i][i].get_text() == '':
-            number_of_x = 0
-            number_of_o = 0
         if game_array[i][i].get_text() == 'O':
-            number_of_x = 0
             number_of_o += 1
         if game_array[i][i].get_text() == 'X':
             number_of_x += 1
-            number_of_o = 0
         if number_of_x == win_condition:
             if level_number % 5 == 0:
                 return 1
@@ -193,105 +192,21 @@ def win(level_number, game_array, array_size, win_condition, catch):
                 return 0
         if number_of_o == win_condition:
             return 3
-        if i == array_size - 1:
-            number_of_x = 0
-            number_of_o = 0
+    number_of_x = 0
+    number_of_o = 0
 
-    for k in range(array_size):
-        j = 0
-        for i in range(array_size - k):
-            if game_array[i][j + k].get_text() == '':
-                number_of_x = 0
-                number_of_o = 0
-            if game_array[i][j + k].get_text() == 'O':
-                number_of_o += 1
-                number_of_x = 0
-            if game_array[i][j + k].get_text() == 'X':
-                number_of_x += 1
-                number_of_o = 0
-            if number_of_x == win_condition:
-                if level_number % 5 == 0:
-                    return 1
-                else:
-                    return 0
-            if number_of_o == win_condition:
-                return 3
-            if i == array_size - k - 1:
-                number_of_x = 0
-                number_of_o = 0
-            j += 1
-
-    for k in range(array_size):
-        j = 0
-        for i in range(array_size - k):
-            if game_array[i + k][j].get_text() == '':
-                number_of_x = 0
-                number_of_o = 0
-            if game_array[i + k][j].get_text() == 'O':
-                number_of_o += 1
-                number_of_x = 0
-            if game_array[i + k][j].get_text() == 'X':
-                number_of_x += 1
-                number_of_o = 0
-            if number_of_x == win_condition:
-                if level_number % 5 == 0:
-                    return 1
-                else:
-                    return 0
-            if number_of_o == win_condition:
-                return 3
-            if i == array_size - k - 1:
-                number_of_x = 0
-                number_of_o = 0
-            j += 1
-
-    for k in range(array_size):
-        j = array_size - 1
-        for i in range(array_size - k):
-            if game_array[i + k][j].get_text() == '':
-                number_of_x = 0
-                number_of_o = 0
-            if game_array[i + k][j].get_text() == 'O':
-                number_of_o += 1
-                number_of_x = 0
-            if game_array[i + k][j].get_text() == 'X':
-                number_of_x += 1
-                number_of_o = 0
-            if number_of_x == win_condition:
-                if level_number % 5 == 0:
-                    return 1
-                else:
-                    return 0
-            if number_of_o == win_condition:
-                return 3
-            if i == array_size - k - 1:
-                number_of_x = 0
-                number_of_o = 0
-            j -= 1
-
-    for k in range(array_size):
-        j = array_size - 1
-        for i in range(array_size - k):
-            if game_array[i][j - k].get_text() == '':
-                number_of_x = 0
-                number_of_o = 0
-            if game_array[i][j - k].get_text() == 'O':
-                number_of_o += 1
-                number_of_x = 0
-            if game_array[i][j - k].get_text() == 'X':
-                number_of_x += 1
-                number_of_o = 0
-            if number_of_x == win_condition:
-                if level_number % 5 == 0:
-                    return 1
-                else:
-                    return 0
-            if number_of_o == win_condition:
-                return 3
-            if i == array_size - k - 1:
-                number_of_x = 0
-                number_of_o = 0
-            j -= 1
+    for i in range(array_size):
+        if game_array[i][array_size - 1 - i].get_text() == 'O':
+            number_of_o += 1
+        if game_array[i][array_size - 1 - i].get_text() == 'X':
+            number_of_x += 1
+        if number_of_x == win_condition:
+            if level_number % 5 == 0:
+                return 1
+            else:
+                return 0
+        if number_of_o == win_condition:
+            return 3
     return catch
 
 
@@ -303,4 +218,3 @@ def draw(domain):
                 full += 1
             if full == 9:
                 return 2
-
