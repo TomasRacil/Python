dcf9c4fd1b2fa2c635a3f8e27dfe4d43b6778dd0
blazkoval <blazkovalenka2@gmail.com>
2022-04-04 15:00:34 +0200

    dynamické vyplnění matice s výroky, upraven výstup,přídány popisy funkce

--- Projekty/VyrokoveTabulky/main.py
+++ Projekty/VyrokoveTabulky/main.py
@@ -1,12 +1,14 @@
 from modules.formule import *
 
-userInput = True    #prenastavit nazacatku na False pro zadavani vyrokovych formuli primo do pragramu
+userInput = True    #přenastavit nazačátku na False pro zadáváni výrokových formulí přímo do programu
 if (userInput):
     print("************************************************************")
     print("Pro zapisování výrokových formulí používat pouze tyto znaky:\n~ \tnegace\n/\ \tkonjunkce\n\/ \tdisjunkce\n==> \timplikace\n<=> \tekvivalence")
-    print("p q r \tpro označení elementárních výroků\n{}[]() \tzávorky\nmezi operací a negací musí být vždy mezera")
+    print("{}[]() \tzávorky\npísmena abecedy (všechna malá i velká) pro označení elementárních výroků\nmezi operací a negací musí být vždy mezera")
     print("************************************************************")
     formule = input("Zadejte výrokovou formuli: ")
-else: formule = ("~p /\ r <=> ~(q ==> (~~~r \/ ~~p))") #sem zadávat formule
-result = Solve(Parsing(formule))
-Vypis(formule,result)
+else: formule = ("~A /\ a <=> ~(q ==> (~~~q \/ ~~p))") #sem zadávat formule
+
+matrices = SetMatrices(formule)
+result = Solve(matrices,Parsing(formule))
+Vypis(matrices,formule,result)
--- Projekty/VyrokoveTabulky/modules/formule.py
+++ Projekty/VyrokoveTabulky/modules/formule.py
@@ -1,23 +1,47 @@
+import re
 from .parser import *
 
-def pqr(s):
-    p = 0
-    q = 0
-    r = 0
-    for znak in s:
-        if znak == "p":
-            p = 1
-        elif znak == "q":
-            q = 1
-        elif znak == "r":
-            r = 1
-    string = (p*'p' + q*'q' + r*'r')
-    return (string)
+#vytvorit dynamicky
+def Vyroky(s):
+    """
+    funkce hledá, jaké výroky se vyskytují v řetezci
+    argumenty: s (string)
+    vrací: vyroky (list)
+    """
+    vyroky=set(re.findall(r'[a-zA-Z]',s))
+    return (sorted(vyroky))
+
+def FillList(amount,step):
+    """
+    naplní pole 0 a 1
+    argumenty: amount (int), step (int)
+    výstup: list (list)
+    """
+    list = []
+    k = 0
+    for i in range(0,2**amount,step):
+        for j in range(step):
+            list.append(k%2)
+        k+=1
+    return list    
+
+def SetMatrices(s):
+    """
+    Vytvoří matici výroků ve formě dictionary... "označení výroku" : pole naplněné 0 a 1
+    argumenty: s (string)
+    vrací: matrices (set)
+    """
+    matrices = {}
+    vyroky = Vyroky(s)
+    steps = 1
+    for vyrok in vyroky:
+        matrices.update({vyrok:FillList(len(vyroky),steps)}) 
+        steps = steps*2
+    return matrices
 
 def Parsing(string):
     string = parse(lex(string))
     return string
-
 
 def Neg(a):
     c = []
@@ -29,7 +53,6 @@ def Neg(a):
             c.append(0)
         x += 1
     return c
-
 def Kon(a, b):
     c = []
     x = 0
@@ -40,8 +63,6 @@ def Kon(a, b):
             c.append(0)
         x += 1
     return c
-
-
 def Dis(a, b):
     c = []
     x = 0
@@ -52,8 +73,6 @@ def Dis(a, b):
             c.append(1)
         x += 1
     return c
-
-
 def Imp(a, b):
     c = []
     x = 0
@@ -64,8 +83,6 @@ def Imp(a, b):
             c.append(1)
         x += 1
     return c
-
-
 def Ekv(a, b):
     c = []
     x = 0
@@ -77,25 +94,22 @@ def Ekv(a, b):
         x += 1
     return c
 
-def Solve(prvek: list) -> list:
-    """Recursively solve ...
-
-    Args:
-        prvek (list): _description_
-
-    Returns:
-        list: _description_
+def Solve(matrices, prvek: list) -> list:
+    """
+    rekurzivní funkce 
+    argumenty: matrices (dictionary), prvek (list)
+    vrací: prvni (list)
     """
     konec = False
     repetition = int(len(prvek) / 2) 
     if isinstance(prvek[0], list):
-        prvni = Solve(prvek.pop(0)) #priradi prvni 0.prvek - tedy list v listu - a smaze ho, zavola znovu funkci solve
+        prvni = Solve(matrices, prvek.pop(0)) #priradi prvni 0.prvek - tedy list v listu - a smaze ho, zavola znovu funkci solve
     else:
         if (prvek[0].endswith('~')):    #pro pripad ze je vice negaci za sebou
             if ((prvek[0].count('~')%2)==1): #negace rusi negaci...sudy pocet negaci = zadna negace
                 if isinstance(prvek[1], list):
                     prvek.pop(0)
-                    prvni = Neg(Solve(prvek.pop(0)))
+                    prvni = Neg(Solve(matrices, prvek.pop(0)))
                     konec = True                                        
                 else:
                     repetition-=1
@@ -112,7 +126,7 @@ def Solve(prvek: list) -> list:
         op = prvek.pop(0) #priradim dalsi prvek operaci a smazu
 
         if isinstance(prvek[0], list):
-            druhy = Solve(prvek.pop(0))
+            druhy = Solve(matrices, prvek.pop(0))
         else:
             if (prvek[0].endswith('~')):
                 k = True
@@ -127,35 +141,27 @@ def Solve(prvek: list) -> list:
         if k: break
     return prvni
 
-def Vypis(formule,result):
+def Vypis(matrices,formule,result):
+    """
+    Vypíše do konzole naformátovanou tabulku pravdivostních hodnot
+    argumenty: matrices (set), formule (string), result (list)
+    """
     print()
-    match pqr(formule):
-        case 'p':                       #kazdy 4. prvek
-            print('p   |  ',formule,"\n––––|–––––––––––––––––––––––––––––––––––––––––––––––––")
-            for i in range(0,8,4):      
-                print(matrices["p"][i],"  |  ",result[i])
-
-        case 'pq':                      #kazdy 2. prvek
-            print("p    q   |  ",formule,"\n–––––––––|––––––––––––––––––––––––––––––––––––––––––––––––––")
-            for i in range(0,8,2):          
-                print(matrices["p"][i],"  ",matrices["q"][i],"  |  ",result[i])
-
-        case _:                         #vsechny prvky
-            print("p    q    r   |  ",formule,"\n––––––––––––––|–––––––––––––––––––––––––––––––––––––––––––––")
-            for i in range(8):                   
-                print(matrices["p"][i],"  ",matrices["q"][i],"  ",matrices["r"][i],"  |  ",result[i])
+    #hlavicka
+    vyroky = Vyroky(formule)
+    o = 0
+    for v in vyroky:
+        print(v,end ="   ")
+        o+=1
+    print("|  ",formule)
+    #oddelovac
+    print(o*"––––",end = "|–––––––––––––––––––––––––––––––––––––––––––––––\n", )
+    #tabulka
+    for i in range(0,2**len(vyroky),1):
+        for j in vyroky:
+            print(matrices[j][i], end = "   ")
+        print("|  ",result[i])
 
 operations = {"and": Kon, "or": Dis, "iff": Ekv, "implies": Imp, "~": Neg}
-
-matrices = {
-    "p": [0, 0, 0, 0, 1, 1, 1, 1],
-    "q": [0, 0, 1, 1, 0, 0, 1, 1],
-    "r": [0, 1, 0, 1, 0, 1, 0, 1],
-}
-
 # ~ negace, /\ konjunkce, \/ disjunkce, ==> implikace, <=> ekvivalence
-
-
-
-
 
