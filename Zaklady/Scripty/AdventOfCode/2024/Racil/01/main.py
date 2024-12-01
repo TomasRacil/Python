105da46bcf94ea7bd2cc05cc3ad3764a1ad39f68
TomasRacil <tomas.racil@unob.cz>
2024-12-01 20:17:33 +0000

    2024 01 AoC

---
+++ Zaklady/Scripty/AdventOfCode/2024/Racil/01/main.py
@@ -0,0 +1,32 @@
+"""
+Solution to 1. day of advent of code 2022
+https://adventofcode.com/2024/day/1
+"""
+
+from os.path import realpath, dirname, join
+
+
+def main():
+    """
+    main
+    """
+    with open(
+        join(dirname(realpath(__file__)), "input.txt"), "r", encoding="utf_8"
+    ) as file:
+        lists = [
+            sorted(l)
+            for l in zip(
+                *[
+                    (int(number) for number in line.split())
+                    for line in file.read().split("\n")
+                ]
+            )
+        ]
+    distances = [abs(item - lists[0][idx]) for idx, item in enumerate(lists[1])]
+    print(sum(distances))
+    simmilarity_scores = [lists[1].count(number) * number for number in lists[0]]
+    print(sum(simmilarity_scores))
+
+
+if __name__ == "__main__":
+    main()
